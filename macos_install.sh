f195c992cc3e0ebebea68113b51c97080c3c003b
Magnesium1062 <Magnesium1062@noreply.codeberg.org>
2024-08-13 21:16:16 -0400

    Up

--- macos_install.sh
+++ macos_install.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 
-mkdir ~/phoenix_temp
+sudo mkdir -p ~/phoenix_temp
 
 cd ~/phoenix_temp
 
 wget https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/mozilla.cfg
 
-mv mozilla.cfg /Applications/Firefox.app/Contents/Resources/mozilla.cfg
+sudo mv mozilla.cfg /Applications/Firefox.app/Contents/Resources/mozilla.cfg
 
 wget https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/defaults/pref/local-settings.js
 
-mkdir /Applications/Firefox.app/Contents/Resources/defaults/pref
+sudo mkdir -p /Applications/Firefox.app/Contents/Resources/defaults/pref
 
-mv local-settings.js /Applications/Firefox.app/Contents/Resources/defaults/pref/local-settings.js
+sudo mv local-settings.js /Applications/Firefox.app/Contents/Resources/defaults/pref/local-settings.js
 
 echo "Make sure to install your policies & enjoy :D"
