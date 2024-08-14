a737d87be963dae1d34ab4cf7a7432523f155bf1
Magnesium1062 <Magnesium1062@noreply.codeberg.org>
2024-08-13 21:30:07 -0400

    Fix!!

--- macos_install.sh
+++ macos_install.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#! /usr/bin/env bash
 
 sudo mkdir -p ~/phoenix_temp
 
@@ -11,6 +11,8 @@ sudo mv mozilla.cfg /Applications/Firefox.app/Contents/Resources/mozilla.cfg
 wget https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/defaults/pref/local-settings.js
 
 sudo mkdir -p /Applications/Firefox.app/Contents/Resources/defaults/pref
+
+sudo chmod 755 /Applications/Firefox.app/Contents/Resources/defaults/pref
 
 sudo mv local-settings.js /Applications/Firefox.app/Contents/Resources/defaults/pref/local-settings.js
 
