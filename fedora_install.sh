675423d6b584318c461104acdd1c2a13be180dba
Magnesium1062 <magnesium1062@noreply.codeberg.org>
2024-08-15 00:55:37 +0000

    Update fedora_install.sh

--- fedora_install.sh
+++ fedora_install.sh
@@ -12,4 +12,6 @@ sudo chmod 755 /usr/lib64/firefox/defaults/pref
 
 sudo mv local-settings.js /usr/lib64/firefox/defaults/pref/local-settings.js
 
-echo "Make sure to install your policies & enjoy :D"
+sudo dnf copr enable retold3202/Phoenix-Policies && sudo dnf update --refresh && sudo dnf install phoenix-policies
+
+printf "All done. :) Congratulations, you've successfully installed Phoenix.\nWhat comes next is for you to decide. I would strongly recommend taking a look at some of the user.js files we offer, such as our 'Hardened' option for more comprehensive protection, at the cost of minimal breakage.\nYou can grab them from here https://codeberg.org/Magnesium1062/Phoenix/src/branch/main/configs. Just drag the user.js under the config of your choice to your profile directory, and enjoy.\n"
