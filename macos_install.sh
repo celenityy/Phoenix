44f421ebc0f3d690d787fd4a012e3de4a9b6627b
Magnesium1062 <Magnesium1062@noreply.codeberg.org>
2024-08-18 18:02:44 -0400

    Initial macOS release :)

--- macos_install.sh
+++ macos_install.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/bin/zsh
 
 wget https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/mozilla.cfg
 
@@ -12,4 +12,14 @@ sudo chmod 755 /Applications/Firefox.app/Contents/Resources/defaults/pref
 
 sudo mv local-settings.js /Applications/Firefox.app/Contents/Resources/defaults/pref/local-settings.js
 
-echo "Make sure to install your policies & enjoy :D"
+brew tap Magnesium1062/Phoenix-Policies-macOS https://codeberg.org/Magnesium1062/Phoenix-Policies-macOS
+
+brew update
+
+brew install phoenix-policies
+
+echo 'alias phoenix-up="cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Application/Firefox.app/Contents/Resources/distribution.policies.json"' >> ~/.zshrc
+
+printf "All done. :) Congratulations, you've successfully installed Phoenix.\nWhat comes next is for you to decide. I would strongly recommend taking a look at some of the user.js files we offer, such as our 'Hardened' option for more comprehensive protection, at the cost of minimal breakage.\nYou can grab them from here https://codeberg.org/Magnesium1062/Phoenix/src/branch/main/configs. Just drag the user.js under the config of your choice to your profile directory, and enjoy.\n"
+
+printf "IMPORTANT: Make sure to run the command "phoenix-up" after you update your policies.\nYou can set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force && phoenix-up'"
