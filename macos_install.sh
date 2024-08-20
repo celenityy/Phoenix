7264e955f89261087f6d378a2d30cdc47ab02d0d
Magnesium1062 <Magnesium1062@noreply.codeberg.org>
2024-08-20 15:12:29 -0400

    Improve macOS installer

--- macos_install.sh
+++ macos_install.sh
@@ -16,12 +16,42 @@ brew tap Magnesium1062/Phoenix-Policies-macOS https://codeberg.org/Magnesium1062
 
 brew update
 
+brew upgrade --force
+
 brew install phoenix-policies
 
 cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json
+
+cat << 'EOF' > /usr/local/sbin/update_policies_phoenix.sh
+#!/bin/zsh
+cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json
+EOF
+
+sudo chmod +x /usr/local/sbin/update_policies_phoenix.sh
+
+cat << 'EOF' > ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.user.updatepoliciesphoenix</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/local/sbin/update_policies_phoenix.sh</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>StartInterval</key>
+    <integer>21600</integer> <!-- 6 hours in seconds -->
+</dict>
+</plist>
+EOF
+
+launchctl load ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist
 
 echo 'alias phoenix-up="cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json"' >> ~/.zshrc
 
 printf "All done. :) Congratulations, you've successfully installed Phoenix.\nWhat comes next is for you to decide. I would strongly recommend taking a look at some of the user.js files we offer, such as our 'Hardened' option for more comprehensive protection, at the cost of minimal breakage.\nYou can grab them from here https://codeberg.org/Magnesium1062/Phoenix/src/branch/main/configs. Just drag the user.js under the config of your choice to your profile directory, and enjoy.\n"
 
-printf "IMPORTANT: Make sure to run the command "phoenix-up" after you update your policies.\nYou can set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force && phoenix-up'"
+printf "NOTE: Due to macOS limitations, by default, your policies will only update every 6 hours or on device boot. If you want to enforce a policies update, you can run "phoenix-up" after the update is downloaded download with Homebrew.\nYou can also set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force && phoenix-up'"
