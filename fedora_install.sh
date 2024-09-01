2142a300b9ceea33601b3094eacd565323f0d639
Seyed Mohamad Amin Modaresi <modaresisofthard@proton.me>
2024-09-01 17:30:15 +0330

    Fix commands output style

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,31 +1,31 @@
 #! /usr/bin/env bash
 
 echo -e "\033[32mDownloading mozilla.cfg\033[0m"
-wget https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/mozilla.cfg
+wget -nv https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/mozilla.cfg
 echo
 
 
 echo -e "\033[32mMoving mozzila.cfg to /usr/lib64/firefox/mozilla.cfg033[0m"
-sudo mv mozilla.cfg /usr/lib64/firefox/mozilla.cfg
+sudo mv -v mozilla.cfg /usr/lib64/firefox/mozilla.cfg
 echo
 
 
 echo -e "\033[32mDownloading local-settings.js\033[0m"
-wget https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/defaults/pref/local-settings.js
+wget -nv https://codeberg.org/Magnesium1062/Phoenix/raw/branch/main/defaults/pref/local-settings.js
 echo
 
 
 echo -e "\033[32mMaking /usr/lib64/firefox/defaults/pref dirctory\033[0m"
-sudo mkdir -p /usr/lib64/firefox/defaults/pref
+sudo mkdir -v -p /usr/lib64/firefox/defaults/pref
 echo
 
 echo -e "\033[32m Changing permisions of /usr/lib64/firefox/defaults/pref to 755\033[0m"
-sudo chmod 755 /usr/lib64/firefox/defaults/pref
+sudo chmod -v 755 /usr/lib64/firefox/defaults/pref
 echo
 
 
 echo -e "\033[32mMoving local-settings.js to /usr/lib64/firefox/defaults/pref/local-settings.js033[0m"
-sudo mv local-settings.js /usr/lib64/firefox/defaults/pref/local-settings.js
+sudo mv -v local-settings.js /usr/lib64/firefox/defaults/pref/local-settings.js
 echo
 
 
