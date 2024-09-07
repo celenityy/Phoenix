286e80a9dfad6d64bd4db5aa3f7e403589c2c083
Magnesium1062 <Magnesium1062@noreply.codeberg.org>
2024-09-07 00:53:57 -0400

    Create macOS Migration Script

---
+++ macos_migration.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+wget https://codeberg.org/celenity/Phoenix/raw/branch/main/mozilla.cfg
+
+sudo mv mozilla.cfg /Applications/Firefox.app/Contents/Resources/mozilla.cfg
+
+brew uninstall phoenix-policies
+
+brew untap Magnesium1062/Phoenix-Policies-macOS
+
+brew tap celenity/Phoenix-Policies-macOS https://codeberg.org/celenity/Phoenix-Policies-macOS
+
+brew update
+
+brew upgrade --force
+
+brew install phoenix-policies
+
+cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json
+
+printf "Thank you for taking the time to run our migration script. Enjoy :)"
