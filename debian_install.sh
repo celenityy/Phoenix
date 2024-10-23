6bac058075f7770d388f39e11d525114015fc526
celenity <celenity@celenity.dev>
2024-10-23 16:08:27 -0400

    Enable HaGeZi's Badware Hoster Blocklist by default & other minor tweaks

--- debian_install.sh
+++ debian_install.sh
@@ -33,47 +33,63 @@ if [ $(id --user) -ne 0 ]; then
 fi
 
 
-echo_green_text "Downloading mozilla.cfg"
+echo_green_text "Downloading mozilla.cfg..."
 wget -nv https://phoenix.celenity.dev/mozilla.cfg || error_fn
 echo
 
 
-echo_green_text "Moving mozilla.cfg to /usr/lib/firefox/mozilla.cfg"
+echo_green_text "Moving mozilla.cfg to /usr/lib/firefox/mozilla.cfg..."
 sudo mv -v mozilla.cfg /usr/lib/firefox/mozilla.cfg || error_fn
 echo
 
 
-echo_green_text "Downloading local-settings.js"
+echo_green_text "Downloading local-settings.js..."
 wget -nv https://phoenix.celenity.dev/defaults/pref/local-settings.js || error_fn
 echo
 
 
-echo_green_text "Creating /usr/lib/firefox/defaults/pref directory"
+echo_green_text "Creating /usr/lib/firefox/defaults/pref directory..."
 sudo mkdir -v -p /usr/lib/firefox/defaults/pref || error_fn
 echo
 
-echo_green_text "Changing permissions of /usr/lib/firefox/defaults/pref to 755"
+echo_green_text "Changing permissions of /usr/lib/firefox/defaults/pref to 755..."
 sudo chmod -v 755 /usr/lib/firefox/defaults/pref || error_fn
 echo
 
 
-echo_green_text "Moving local-settings.js to /usr/lib/firefox/defaults/pref/local-settings.js"
+echo_green_text "Moving local-settings.js to /usr/lib/firefox/defaults/pref/local-settings.js..."
 sudo mv -v local-settings.js /usr/lib/firefox/defaults/pref/local-settings.js || error_fn
 echo
 
 
-echo_green_text "Adding Prebuilt MPR repo if not already installed"
+echo_green_text "Adding Prebuilt MPR repo if not already installed..."
 curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
 echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr ${Release_CodeName}" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
 echo
 
-echo_green_text "Updating APT cache"
+echo_green_text "Updating APT cache..."
 sudo apt update || error_fn
 echo
 
+echo_green_text "Installing Makedeb if not already installed..."
+sudo apt install makedeb || error_fn
+echo
 
-echo_green_text "Installing phoenix-policies package"
-sudo apt install phoenix-policies || error_fn
+echo_green_text "Installing git if not already installed..."
+sudo apt install git || error_fn
+echo
+
+echo_green_text "Cloning Mist..."
+git clone "https://mpr.makedeb.org/mist.git" || error_fn
+echo
+
+echo_green_text "Building & Installing Mist..."
+cd mist/
+makedeb -s -i
+echo
+
+echo_green_text "Installing phoenix-policies package..."
+mist phoenix-policies || error_fn
 echo
 
 
