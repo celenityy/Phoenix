16007fb3dc3e495757452aa20dc65f66177cb7e9
celenity <celenity@celenity.dev>
2024-11-30 16:26:57 -0500

    Harden security of local-settings.js/our mozilla.cfg
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/local-settings.js
+++ defaults/pref/local-settings.js
@@ -1,3 +1,7 @@
 pref("general.config.filename", "mozilla.cfg");
 
 pref("general.config.obscure_value", 0);
+
+// Enforce that we (or any other mozilla.cfg files) do not get privileged browser access...
+// https://www.mozilla.org/firefox/62.0/releasenotes/
+pref("general.config.sandbox_enabled", true);
