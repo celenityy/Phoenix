f9d7a27c45cd8ec3193eaba891552461897f67b6
celenity <celenity@celenity.dev>
2024-12-01 17:09:56 -0500

    Rename local-settings.js to phoenix.js & mozilla.cfg to phoenix.cfg. Also remove the need for having a separate local-settings.js file.
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/local-settings.js
+++
@@ -1,7 +0,0 @@
-pref("general.config.filename", "mozilla.cfg");
-
-pref("general.config.obscure_value", 0);
-
-// Enforce that we (or any other mozilla.cfg files) do not get privileged browser access...
-// https://www.mozilla.org/firefox/62.0/releasenotes/
-pref("general.config.sandbox_enabled", true);
--- defaults/pref/phoenix.js
+++ defaults/pref/phoenix.js
@@ -1,7 +1,6 @@
-pref("general.config.filename", "base.cfg");
-
-pref("general.config.obscure_value", 0);
-
 // Enforce that we (or any other mozilla.cfg files) do not get privileged browser access...
 // https://www.mozilla.org/firefox/62.0/releasenotes/
 pref("general.config.sandbox_enabled", true);
+
+pref("general.config.filename", "phoenix.cfg");
+pref("general.config.obscure_value", 0);
