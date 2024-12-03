91a7e073ffbbfa4d4f4573299c439e82f574d5a4
celenity <celenity@celenity.dev>
2024-12-03 16:42:33 -0500

    Harden our cfg files further than before for defense in depth against potential attacks or compromise
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/phoenix.js
+++ defaults/pref/phoenix.js
@@ -2,6 +2,10 @@
 // https://www.mozilla.org/firefox/62.0/releasenotes/
 pref("general.config.sandbox_enabled", true);
 
+// Defense in depth, enforces users can never be identified...
+pref("mail.identity.useremail", "");
+pref("autoadmin.append_emailaddr", false);
+
 pref("general.config.filename", "phoenix.cfg");
 pref("general.config.vendor", "phoenix");
 pref("general.config.obscure_value", 0);
