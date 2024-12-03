e61d7b110ac6c1cfde70602629debb76dca997ce
celenity <celenity@celenity.dev>
2024-12-03 03:28:06 -0500

    Update
    Signed-off-by: celenity <celenity@celenity.dev>

--- policies/gen_all.sh
+++ policies/gen_all.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 
-jq -s '.[0] * .[1]' Linux/Policies/base-policies.json Blocklist/blocklist.json > Linux/Policies/temp.json
+jq -s '.[0] * .[1]' policies/Linux/Policies/base-policies.json policies/Blocklist/blocklist.json > policies/Linux/Policies/temp.json
 
-jq -s '.[0] * .[1]' Linux/Policies/temp.json Blocklist/cookies.json > Linux/Policies/policies.json
+jq -s '.[0] * .[1]' policies/Linux/Policies/temp.json policies/Blocklist/cookies.json > policies/Linux/Policies/policies.json
 
-rm -f Linux/Policies/temp.json
+rm -f policies/Linux/Policies/temp.json
 
-jq -s '.[0] * .[1]' Linux/Policies/policies.json Policies/policies-specific.json > Policies/policies.json
+jq -s '.[0] * .[1]' policies/Linux/Policies/policies.json policies/Policies/policies-specific.json > policies/Policies/policies.json
 
-jq -s '.[0] * .[1]' Linux/Policies/policies.json No-Sync/policies-specific.json > Linux/No-Sync/policies.json
+jq -s '.[0] * .[1]' policies/Linux/Policies/policies.json policies/No-Sync/policies-specific.json > policies/Linux/No-Sync/policies.json
 
-jq -s '.[0] * .[1]' Linux/No-Sync/policies.json Policies/policies-specific.json > No-Sync/policies.json
+jq -s '.[0] * .[1]' policies/Linux/No-Sync/policies.json policies/Policies/policies-specific.json > policies/No-Sync/policies.json
 
-jq -s '.[0] * .[1]' Linux/Policies/policies.json Personal-Policies/policies-specific.json > Linux/Personal-Policies/policies.json
+jq -s '.[0] * .[1]' policies/Linux/Policies/policies.json policies/Personal-Policies/policies-specific.json > policies/Linux/Personal-Policies/policies.json
 
-jq -s '.[0] * .[1]' Linux/Personal-Policies/policies.json /Policies/policies-specific.json > Personal-Policies/temp.json
+jq -s '.[0] * .[1]' policies/Linux/Personal-Policies/policies.json policies/Policies/policies-specific.json > policies/Personal-Policies/temp.json
 
-jq -s '.[0] * .[1]' Personal-Policies/temp.json Personal-Policies/enforce-updating.json > Personal-Policies/policies.json
+jq -s '.[0] * .[1]' policies/Personal-Policies/temp.json policies/Personal-Policies/enforce-updating.json > policies/Personal-Policies/policies.json
 
-rm -f Personal-Policies/temp.json
+rm -f policies/Personal-Policies/temp.json
 
-jq -s '.[0] * .[1]' Linux/Personal-Policies/policies.json No-Sync/policies-specific.json > Linux/Personal-Dev-Nightly-Policies/temp.json
+jq -s '.[0] * .[1]' policies/Linux/Personal-Policies/policies.json policies/No-Sync/policies-specific.json > policies/Linux/Personal-Dev-Nightly-Policies/temp.json
 
-jq -s '.[0] * .[1]' Linux/Personal-Dev-Nightly-Policies/temp.json Personal-Dev-Nightly-Policies/policies-specific.json > Linux/Personal-Dev-Nightly-Policies/policies.json
+jq -s '.[0] * .[1]' policies/Linux/Personal-Dev-Nightly-Policies/temp.json policies/Personal-Dev-Nightly-Policies/policies-specific.json > policies/Linux/Personal-Dev-Nightly-Policies/policies.json
 
-rm -f Linux/Personal-Dev-Nightly-Policies/temp.json
+rm -f policies/Linux/Personal-Dev-Nightly-Policies/temp.json
 
-jq -s '.[0] * .[1]' Linux/Personal-Dev-Nightly-Policies/policies.json Policies/policies-specific.json > Personal-Dev-Nightly-Policies/temp.json
+jq -s '.[0] * .[1]' policies/Linux/Personal-Dev-Nightly-Policies/policies.json policies/Policies/policies-specific.json > policies/Personal-Dev-Nightly-Policies/temp.json
 
-jq -s '.[0] * .[1]' Personal-Dev-Nightly-Policies/temp.json Personal-Policies/enforce-updating.json > Personal-Dev-Nightly-Policies/policies.json
+jq -s '.[0] * .[1]' policies/Personal-Dev-Nightly-Policies/temp.json policies/Personal-Policies/enforce-updating.json > policies/Personal-Dev-Nightly-Policies/policies.json
 
-rm -f Personal-Dev-Nightly-Policies/temp.json
+rm -f policies/Personal-Dev-Nightly-Policies/temp.json
 
-# Replace {fedir} with the directory where Phoenix-Policies-Fedora is located, otherwise you can comment this out
-cp Linux/Policies/policies.json {fedir}/standard/policies.json
+# Replace ~/Projects/Phoenix-Policies-Fedora with the directory where Phoenix-Policies-Fedora is located, otherwise you can comment this out
+cp policies/Linux/Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/standard/policies.json
 
-cp Linux/No-Sync/policies.json {fedir}/no-sync/policies.json
+cp policies/Linux/No-Sync/policies.json ~/Projects/Phoenix-Policies-Fedora/no-sync/policies.json
 
-cp Linux/Personal-Policies/policies.json {fedir}/personal/policies.json
+cp policies/Linux/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/personal/policies.json
 
-cp Linux/Personal-Dev-Nightly-Policies/policies.json {fedir}/personal-dev-nightly/policies.json
+cp policies/Linux/Personal-Dev-Nightly-Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/personal-dev-nightly/policies.json
 
-# Replace {dedir} with the directory where Phoenix-Policies-Debian is located, otherwise you can comment this out
-cp Linux/Policies/policies.json {dedir}/phoenix-policies/policies.json
+# Replace ~/Projects/Phoenix-Policies-Debian with the directory where Phoenix-Policies-Debian is located, otherwise you can comment this out
+cp policies/Linux/Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies/policies.json
 
-cp Linux/No-Sync/policies.json {dedir}/phoenix-policies-no-sync/policies.json
+cp policies/Linux/No-Sync/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-no-sync/policies.json
 
-cp Linux/Personal-Policies/policies.json {dedir}/phoenix-policies-personal/policies.json
+cp policies/Linux/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-personal/policies.json
