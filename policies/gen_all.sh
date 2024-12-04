b595bec2412304cb45129e6f4f68d7f3b2ac560d
celenity <celenity@celenity.dev>
2024-12-04 02:25:56 -0500

    Organize
    Signed-off-by: celenity <celenity@celenity.dev>

--- policies/gen_all.sh
+++ policies/gen_all.sh
@@ -8,18 +8,18 @@ rm -f policies/Policies/temp.json
 
 jq -s '.[0] * .[1]' policies/Policies/policies.json policies/No-Sync/policies-specific.json > policies/No-Sync/policies.json
 
-jq -s '.[0] * .[1]' policies/Policies/policies.json policies/Personal-Policies/policies-specific.json > policies/Personal-Policies/policies.json
+jq -s '.[0] * .[1]' policies/Policies/policies.json policies/Personal/policies-specific.json > policies/Personal/policies.json
 
 # Replace ~/Projects/Phoenix-Policies-Fedora with the directory where Phoenix-Policies-Fedora is located, otherwise you can comment this section out
 cp policies/Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/standard/policies.json
 
 cp policies/No-Sync/policies.json ~/Projects/Phoenix-Policies-Fedora/no-sync/policies.json
 
-cp policies/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/personal/policies.json
+cp policies/Personal/policies.json ~/Projects/Phoenix-Policies-Fedora/personal/policies.json
 
 # Replace ~/Projects/Phoenix-Policies-Debian with the directory where Phoenix-Policies-Debian is located, otherwise you can comment this section out
 cp policies/Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies/policies.json
 
 cp policies/No-Sync/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-no-sync/policies.json
 
-cp policies/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-personal/policies.json
+cp policies/Personal/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-personal/policies.json
