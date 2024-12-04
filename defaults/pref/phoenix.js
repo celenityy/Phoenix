452bf4a974e7ea9431088a1dd3cac489b9c8ba83
celenity <celenity@celenity.dev>
2024-12-04 02:15:29 -0500

    Lots of updates here:
    * Hardened Phoenix's local and remote config files to prevent us or an attacker (if we ever compromised) from having the ability to override the user's homepage. Downside is that users will no longer be able to easily set a different browser homepage via `about:preferences`, however they can still set an override via editing their local 'phoenix.cfg', which will persist as we don't remotely update it. See here: https://codeberg.org/celenity/Phoenix/issues/18#issuecomment-2490737 for more details on Phoenix's approach to security and protecting users, even in the worst case scenarios, and even from ourselves. Due to these updates, you are HIGHLY recommend to re-install Phoenix (specifically, the local 'phoenix.cfg' file and 'phoenix.js' files should be updated).
    
    * Placed further restrictions on the installation of extensions, to ensure that we're only granting this capability to websites (with user permission) where it's absolutely necessary.
    
    * Fixed a strange bug that prevented users from installing extensions from the GitHub repos we allow in certain instances (primarily if the .xpi was file was on a past page of releases)
    
    * Heavy consolidated policies. There are no longer separate policies for Windows/macOS & Linux. 'Personal-Dev-Nightly-Policies' have also been fully deprecated. Users can now choose from either our Standard Policies, or 'No-Sync' (or my Personal policies at their own discretion....).
    
    * Added policies to ensure that the user can always access about:addons, about:config, about:profiles, & about:support.
    
    * Added a policy to always display the bookmark toolbar by default. Can be overriden by users depending on their preference.
    
    * Added a policy to ensure that users are always notified if there is an attempt to activate dangerous, invasive 'Content Analysis' functionality.
    
    * Firefox will no longer offer to save passwords for users by default. Can be overriden, though users are highly recommended to use secure password managers instead, such as Bitwarden or Proton Pass.
    
    * Fully disabled Password Manager on my Personal Policies & set to install Bitwarden by default
    
    * Disabled the annoying 'Profile Import' functionality on my Personal Policies
    
    * Added a 'Phoenix Policies Initialized' policy, as well as 'Phoenix Initialized' & 'Phoenix Applied' prefs, to make Phoenix easier to debug & confirm whether it is working as expected.
    
    * Various other minor tweaks & enhancements
    
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/phoenix.js
+++ defaults/pref/phoenix.js
@@ -1,4 +1,6 @@
-// Enforce that we (or any other mozilla.cfg files) do not get privileged browser access...
+//
+pref("0.phoenix.js.initialized", true);
+// Enforce that we(or any other mozilla.cfg files) do not get privileged browser access...
 // https://www.mozilla.org/firefox/62.0/releasenotes/
 pref("general.config.sandbox_enabled", true);
 
@@ -11,3 +13,5 @@ pref("autoadmin.append_emailaddr", false);
 pref("general.config.filename", "phoenix.cfg");
 pref("general.config.vendor", "phoenix");
 pref("general.config.obscure_value", 0);
+
+pref("0.phoenix.js.applied", true);
--- policies/gen_all.sh
+++ policies/gen_all.sh
@@ -1,49 +1,25 @@
 #!/bin/bash
 
-jq -s '.[0] * .[1]' policies/Linux/Policies/base-policies.json policies/Blocklist/blocklist.json > policies/Linux/Policies/temp.json
+jq -s '.[0] * .[1]' policies/Policies/base-policies.json policies/Blocklist/blocklist.json > policies/Policies/temp.json
 
-jq -s '.[0] * .[1]' policies/Linux/Policies/temp.json policies/Blocklist/cookies.json > policies/Linux/Policies/policies.json
+jq -s '.[0] * .[1]' policies/Policies/temp.json policies/Blocklist/cookies.json > policies/Policies/policies.json
 
-rm -f policies/Linux/Policies/temp.json
+rm -f policies/Policies/temp.json
 
-jq -s '.[0] * .[1]' policies/Linux/Policies/policies.json policies/Policies/policies-specific.json > policies/Policies/policies.json
+jq -s '.[0] * .[1]' policies/Policies/policies.json policies/No-Sync/policies-specific.json > policies/No-Sync/policies.json
 
-jq -s '.[0] * .[1]' policies/Linux/Policies/policies.json policies/No-Sync/policies-specific.json > policies/Linux/No-Sync/policies.json
+jq -s '.[0] * .[1]' policies/Policies/policies.json policies/Personal-Policies/policies-specific.json > policies/Personal-Policies/policies.json
 
-jq -s '.[0] * .[1]' policies/Linux/No-Sync/policies.json policies/Policies/policies-specific.json > policies/No-Sync/policies.json
+# Replace ~/Projects/Phoenix-Policies-Fedora with the directory where Phoenix-Policies-Fedora is located, otherwise you can comment this section out
+cp policies/Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/standard/policies.json
 
-jq -s '.[0] * .[1]' policies/Linux/Policies/policies.json policies/Personal-Policies/policies-specific.json > policies/Linux/Personal-Policies/policies.json
+cp policies/No-Sync/policies.json ~/Projects/Phoenix-Policies-Fedora/no-sync/policies.json
 
-jq -s '.[0] * .[1]' policies/Linux/Personal-Policies/policies.json policies/Policies/policies-specific.json > policies/Personal-Policies/temp.json
+cp policies/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/personal/policies.json
 
-jq -s '.[0] * .[1]' policies/Personal-Policies/temp.json policies/Personal-Policies/enforce-updating.json > policies/Personal-Policies/policies.json
+# Replace ~/Projects/Phoenix-Policies-Debian with the directory where Phoenix-Policies-Debian is located, otherwise you can comment this section out
+cp policies/Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies/policies.json
 
-rm -f policies/Personal-Policies/temp.json
+cp policies/No-Sync/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-no-sync/policies.json
 
-jq -s '.[0] * .[1]' policies/Linux/Personal-Policies/policies.json policies/No-Sync/policies-specific.json > policies/Linux/Personal-Dev-Nightly-Policies/temp.json
-
-jq -s '.[0] * .[1]' policies/Linux/Personal-Dev-Nightly-Policies/temp.json policies/Personal-Dev-Nightly-Policies/policies-specific.json > policies/Linux/Personal-Dev-Nightly-Policies/policies.json
-
-rm -f policies/Linux/Personal-Dev-Nightly-Policies/temp.json
-
-jq -s '.[0] * .[1]' policies/Linux/Personal-Dev-Nightly-Policies/policies.json policies/Policies/policies-specific.json > policies/Personal-Dev-Nightly-Policies/temp.json
-
-jq -s '.[0] * .[1]' policies/Personal-Dev-Nightly-Policies/temp.json policies/Personal-Policies/enforce-updating.json > policies/Personal-Dev-Nightly-Policies/policies.json
-
-rm -f policies/Personal-Dev-Nightly-Policies/temp.json
-
-# Replace ~/Projects/Phoenix-Policies-Fedora with the directory where Phoenix-Policies-Fedora is located, otherwise you can comment this out
-cp policies/Linux/Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/standard/policies.json
-
-cp policies/Linux/No-Sync/policies.json ~/Projects/Phoenix-Policies-Fedora/no-sync/policies.json
-
-cp policies/Linux/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/personal/policies.json
-
-cp policies/Linux/Personal-Dev-Nightly-Policies/policies.json ~/Projects/Phoenix-Policies-Fedora/personal-dev-nightly/policies.json
-
-# Replace ~/Projects/Phoenix-Policies-Debian with the directory where Phoenix-Policies-Debian is located, otherwise you can comment this out
-cp policies/Linux/Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies/policies.json
-
-cp policies/Linux/No-Sync/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-no-sync/policies.json
-
-cp policies/Linux/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-personal/policies.json
+cp policies/Personal-Policies/policies.json ~/Projects/Phoenix-Policies-Debian/phoenix-policies-personal/policies.json
