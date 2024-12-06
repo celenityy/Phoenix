b099c67222cb10885fe5ccb9d8b4f1599dbfcf06
celenity <celenity@celenity.dev>
2024-12-06 00:57:33 -0500

    Update install & uninstall script, https://codeberg.org/celenity/Phoenix/pulls/25
    Signed-off-by: celenity <celenity@celenity.dev>

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 coloroff='\e[0m';
-black='\e[0;30m';
 blue='\e[0;34m';
 cyan='\e[0;36m';
 green='\e[0;32m';
@@ -36,141 +35,41 @@ echo -e "${red}To begin, please choose your operating system.${coloroff}";
 echo -e "";
 echo -e "${purple}Your options are:${coloroff}";
 echo -e "";
-echo -e "${cyan}arch${coloroff} - ${red}Arch Linux${coloroff}";
-echo -e "${yellow}debian${coloroff} - ${red}Debian (& Derivatives, such as Ubuntu)${coloroff}";
-echo -e "${blue}fedora${coloroff} - ${red}Fedora Linux${coloroff}";
-echo -e "${white}macOS${coloroff} - ${red}macOS${coloroff}";
+echo -e "${cyan}arch${coloroff} - ${green}Arch Linux${coloroff}";
+echo -e "${yellow}debian${coloroff} - ${green}Debian (& Derivatives, such as Ubuntu)${coloroff}";
+echo -e "${blue}fedora${coloroff} - ${green}Fedora Linux${coloroff}";
+echo -e "${white}macOS${coloroff} - ${green}macOS${coloroff}";
 read -p 'Enter your selection: ' DISTRO
 
 case ${DISTRO} in
 
-	"arch")
+	"arch" | "Arch" | "ARCH")
 		echo -e "${red}Please choose your AUR helper.${coloroff}";
 		echo -e "";
 		echo -e "${purple}Your options are:${coloroff}";
 		echo -e "";
-		echo -e "${blue}paru${coloroff} - ${red}Paru${coloroff}";
-		echo -e "${yellow}yay${coloroff} - ${red}Yay (Yet Another Yogurt)${coloroff}";
+		echo -e "${blue}paru${coloroff} - ${green}Paru${coloroff}";
+		echo -e "${yellow}yay${coloroff} - ${green}Yay (Yet Another Yogurt)${coloroff}";
 		read -p 'Enter your selection: ' HELPER
 		case ${HELPER} in
-			"paru")
+			"paru" | "Paru" | "PARU")
 				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
 				;;
-			"Paru")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"PARU")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"yay")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"Yay")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"YAY")
+			"yay" | "Yay" | "YAY")
 				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
 				;;
 		esac
 		;;
 
-	"Arch")
-		echo -e "${red}Please choose your AUR helper.${coloroff}";
-		echo -e "";
-		echo -e "${purple}Your options are:${coloroff}";
-		echo -e "";
-		echo -e "${blue}paru${coloroff} - ${red}Paru${coloroff}";
-		echo -e "${yellow}yay${coloroff} - ${red}Yay (Yet Another Yogurt)${coloroff}";
-		read -p 'Enter your selection: ' HELPER
-		case ${HELPER} in
-			"paru")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"Paru")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"PARU")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"yay")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"Yay")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"YAY")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-		esac
-		;;
-
-	"ARCH")
-		echo -e "${red}Please choose your AUR helper.${coloroff}";
-		echo -e "";
-		echo -e "${purple}Your options are:${coloroff}";
-		echo -e "";
-		echo -e "${blue}paru${coloroff} - ${red}Paru${coloroff}";
-		echo -e "${yellow}yay${coloroff} - ${red}Yay (Yet Another Yogurt)${coloroff}";
-		read -p 'Enter your selection: ' HELPER
-		case ${HELPER} in
-			"paru")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"Paru")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"PARU")
-				install_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"yay")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"Yay")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"YAY")
-				install_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-		esac
-		;;
-
-	"debian")
+	"debian" | "Debian" | "DEBIAN")
 		install_phoenix ${URL}/${SCRIPT[2]} ${SCRIPT[2]}
 		;;
 
-	"Debian")
-		install_phoenix ${URL}/${SCRIPT[2]} ${SCRIPT[2]}
-		;;
-
-	"DEBIAN")
-		install_phoenix ${URL}/${SCRIPT[2]} ${SCRIPT[2]}
-		;;
-
-	"fedora")
+	"fedora" | "Fedora" | "FEDORA")
 		install_phoenix ${URL}/${SCRIPT[3]} ${SCRIPT[3]}
 		;;
 
-	"Fedora")
-		install_phoenix ${URL}/${SCRIPT[3]} ${SCRIPT[3]}
-		;;
-
-	"FEDORA")
-		install_phoenix ${URL}/${SCRIPT[3]} ${SCRIPT[3]}
-		;;
-
-	"macOS")
-		install_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
-		;;
-
-	"macos")
-		install_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
-		;;
-
-	"MacOS")
-		install_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
-		;;
-
-	"MACOS")
+	"macOS" | "macos" | "MacOS" | "MACOS")
 		install_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
 		;;
 esac
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 coloroff='\e[0m';
-black='\e[0;30m';
 blue='\e[0;34m';
 cyan='\e[0;36m';
 green='\e[0;32m';
@@ -38,141 +37,41 @@ echo -e "${red}To begin, please choose your operating system.${coloroff}";
 echo -e "";
 echo -e "${purple}Your options are:${coloroff}";
 echo -e "";
-echo -e "${cyan}arch${coloroff} - ${red}Arch Linux${coloroff}";
-echo -e "${yellow}debian${coloroff} - ${red}Debian (& Derivatives, such as Ubuntu)${coloroff}";
-echo -e "${blue}fedora${coloroff} - ${red}Fedora Linux${coloroff}";
-echo -e "${white}macOS${coloroff} - ${red}macOS${coloroff}";
+echo -e "${cyan}arch${coloroff} - ${green}Arch Linux${coloroff}";
+echo -e "${yellow}debian${coloroff} - ${green}Debian (& Derivatives, such as Ubuntu)${coloroff}";
+echo -e "${blue}fedora${coloroff} - ${green}Fedora Linux${coloroff}";
+echo -e "${white}macOS${coloroff} - ${green}macOS${coloroff}";
 read -p 'Enter your selection: ' DISTRO
 
 case ${DISTRO} in
 
-	"arch")
+	"arch" | "Arch" | "ARCH")
 		echo -e "${red}Please choose your AUR helper.${coloroff}";
 		echo -e "";
 		echo -e "${purple}Your options are:${coloroff}";
 		echo -e "";
-		echo -e "${blue}paru${coloroff} - ${red}Paru${coloroff}";
-		echo -e "${yellow}yay${coloroff} - ${red}Yay (Yet Another Yogurt)${coloroff}";
+		echo -e "${blue}paru${coloroff} - ${green}Paru${coloroff}";
+		echo -e "${yellow}yay${coloroff} - ${green}Yay (Yet Another Yogurt)${coloroff}";
 		read -p 'Enter your selection: ' HELPER
 		case ${HELPER} in
-			"paru")
+			"paru" | "Paru" | "PARU")
 				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
 				;;
-			"Paru")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"PARU")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"yay")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"Yay")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"YAY")
+			"yay" | "Yay" | "YAY")
 				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
 				;;
 		esac
 		;;
 
-	"Arch")
-		echo -e "${red}Please choose your AUR helper.${coloroff}";
-		echo -e "";
-		echo -e "${purple}Your options are:${coloroff}";
-		echo -e "";
-		echo -e "${blue}paru${coloroff} - ${red}Paru${coloroff}";
-		echo -e "${yellow}yay${coloroff} - ${red}Yay (Yet Another Yogurt)${coloroff}";
-		read -p 'Enter your selection: ' HELPER
-		case ${HELPER} in
-			"paru")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"Paru")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"PARU")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"yay")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"Yay")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"YAY")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-		esac
-		;;
-
-	"ARCH")
-		echo -e "${red}Please choose your AUR helper.${coloroff}";
-		echo -e "";
-		echo -e "${purple}Your options are:${coloroff}";
-		echo -e "";
-		echo -e "${blue}paru${coloroff} - ${red}Paru${coloroff}";
-		echo -e "${yellow}yay${coloroff} - ${red}Yay (Yet Another Yogurt)${coloroff}";
-		read -p 'Enter your selection: ' HELPER
-		case ${HELPER} in
-			"paru")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"Paru")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"PARU")
-				uninstall_phoenix ${URL}/${SCRIPT[0]} ${SCRIPT[0]}
-				;;
-			"yay")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"Yay")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-			"YAY")
-				uninstall_phoenix ${URL}/${SCRIPT[1]} ${SCRIPT[1]}
-				;;
-		esac
-		;;
-
-	"debian")
+	"debian" | "Debian" | "DEBIAN")
 		uninstall_phoenix ${URL}/${SCRIPT[2]} ${SCRIPT[2]}
 		;;
 
-	"Debian")
-		uninstall_phoenix ${URL}/${SCRIPT[2]} ${SCRIPT[2]}
-		;;
-
-	"DEBIAN")
-		uninstall_phoenix ${URL}/${SCRIPT[2]} ${SCRIPT[2]}
-		;;
-
-	"fedora")
+	"fedora"| "Fedora" | "FEDORA")
 		uninstall_phoenix ${URL}/${SCRIPT[3]} ${SCRIPT[3]}
 		;;
 
-	"Fedora")
-		uninstall_phoenix ${URL}/${SCRIPT[3]} ${SCRIPT[3]}
-		;;
-
-	"FEDORA")
-		uninstall_phoenix ${URL}/${SCRIPT[3]} ${SCRIPT[3]}
-		;;
-
-	"macOS")
-		uninstall_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
-		;;
-
-	"macos")
-		uninstall_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
-		;;
-
-	"MacOS")
-		uninstall_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
-		;;
-
-	"MACOS")
+	"macOS" | "macos" | "MacOS" | "MACOS")
 		uninstall_phoenix ${URL}/${SCRIPT[4]} ${SCRIPT[4]}
 		;;
 esac
