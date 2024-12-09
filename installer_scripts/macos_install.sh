8a172566a65518e6d39f2f80b3d247024df27ef8
celenity <celenity@celenity.dev>
2024-12-09 02:00:10 -0500

    Heavily improve macOS installation & uninstallation scripts
    Signed-off-by: celenity <celenity@celenity.dev>

--- installer_scripts/macos_install.sh
+++ installer_scripts/macos_install.sh
@@ -1,34 +1,80 @@
 #!/bin/zsh
 
-wget https://phoenix.celenity.dev/phoenix.cfg
 
-sudo mv phoenix.cfg /Applications/Firefox.app/Contents/Resources/phoenix.cfg
+## Downloaded files save in /tmp for moving
+cd /tmp
 
-wget https://phoenix.celenity.dev/defaults/pref/phoenix.js
 
-sudo mkdir -p /Applications/Firefox.app/Contents/Resources/defaults/pref
+## Functions
+echo_red_text() {
+	echo -e "\033[31m$1\033[0m"
+}
 
-sudo chmod 655 /Applications/Firefox.app/Contents/Resources/defaults/pref
+echo_green_text() {
+	echo -e "\033[32m$1\033[0m"
+}
 
-sudo mv phoenix.js /Applications/Firefox.app/Contents/Resources/defaults/pref/phoenix.js
+error_fn() {
+	echo
+	echo -e "\033[31mSomething went wrong! The script failed.\033[0m"
+	echo -e "\033[31mPlease report this (with the output message) to https://phoenix.celenity.dev/issues\033[0m"
+	echo
+	exit 1
+}
 
-brew tap celenity/tap https://codeberg.org/celenity/tap
+## Install Phoenix
+echo_green_text "Downloading phoenix.cfg..."
+wget -nv https://phoenix.celenity.dev/phoenix.cfg || error_fn
+echo
 
-brew update
+echo_green_text "Moving phoenix.cfg to /Applications/Firefox.app/Contents/Resources/phoenix.cfg..."
+sudo mv -v phoenix.cfg /Applications/Firefox.app/Contents/Resources/phoenix.cfg || error_fn
+echo
 
-brew upgrade --force
+echo_green_text "Downloading phoenix.js..."
+wget -nv https://phoenix.celenity.dev/defaults/pref/phoenix.js || error_fn
+echo
 
-brew install phoenix-policies
+echo_green_text "Creating /Applications/Firefox.app/Contents/Resources/defaults/pref directory..."
+sudo mkdir -v -p /Applications/Firefox.app/Contents/Resources/defaults/pref || error_fn
+echo
 
-cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json
+echo_green_text "Changing permissions of /Applications/Firefox.app/Contents/Resources/defaults/pref to 655..."
+sudo chmod -v 655 /Applications/Firefox.app/Contents/Resources/defaults/pref || error_fn
+echo
 
+echo_green_text "Moving phoenix.js to /Applications/Firefox.app/Contents/Resources/defaults/pref/phoenix.js..."
+sudo mv -v phoenix.js /Applications/Firefox.app/Contents/Resources/defaults/pref/phoenix.js || error_fn
+echo
+
+echo_green_text "Adding celenity's Tap to Homebrew..."
+brew tap celenity/tap https://codeberg.org/celenity/tap || error_fn
+echo
+
+echo_green_text "Updating Homebrew cache..."
+brew update && brew upgrade --force --verbose || error_fn
+echo
+
+echo_green_text "Installing phoenix-policies package..."
+brew install phoenix-policies || error_fn
+echo
+
+echo_green_text "Moving Phoenix's policies.json from /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json to /Applications/Firefox.app/Contents/Resources/distribution/policies.json..."
+cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json || error_fn
+echo
+
+echo_green_text "Creating a script to automatically copy Phoenix's policies.json from /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json to /Applications/Firefox.app/Contents/Resources/distribution/policies.json for updates..."
 cat << 'EOF' > /usr/local/sbin/update_policies_phoenix.sh
 #!/bin/zsh
 cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json
-EOF
+EOF || error_fn
+echo
 
-sudo chmod +x /usr/local/sbin/update_policies_phoenix.sh
+echo_green_text "Making /usr/local/sbin/update_policies_phoenix.sh executable..."
+sudo chmod +x /usr/local/sbin/update_policies_phoenix.sh || error_fn
+echo
 
+echo_green_text "Creating a launch agent to automatically copy Phoenix's policies.json from /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json to /Applications/Firefox.app/Contents/Resources/distribution/policies.json for updates..."
 cat << 'EOF' > ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -46,12 +92,17 @@ cat << 'EOF' > ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist
     <integer>21600</integer> <!-- 6 hours in seconds -->
 </dict>
 </plist>
-EOF
+EOF || error_fn
+echo
 
-launchctl load ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist
+echo_green_text "Loading  ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist..."
+launchctl load ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist || error_fn
+echo
 
-echo 'alias phoenix-up="cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json"' >> ~/.zshrc
+echo "Setting a 'phoenix-up' alias to easily update Phoenix's policies..."
+echo 'alias phoenix-up="cp /opt/homebrew/opt/phoenix-policies/etc/firefox/distribution/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json"' >> ~/.zshrc | error_fn
+echo
 
-printf "All done. :) Congratulations, you've successfully installed Phoenix.\nWhat comes next is for you to decide. I would strongly recommend taking a look at some of the user.js files we offer, such as our 'Hardened' option for more comprehensive protection, at the cost of minimal breakage.\nYou can learn more here https://phoenix.celenity.dev/#complete-coverage.\n"
+echo_green_text "All done. :) Congratulations, you've successfully installed Phoenix.\nWhat comes next is for you to decide. I would strongly recommend taking a look at some of the user.js files we offer, such as our 'Hardened' option for more comprehensive protection, at the cost of minimal breakage.\nYou can learn more here https://phoenix.celenity.dev/#complete-coverage.\n"
 
-printf "NOTE: Due to macOS limitations, by default, your policies will only update every 6 hours or on device boot. If you want to enforce a policies update, you can run "phoenix-up" after the update is downloaded download with Homebrew.\nYou can also set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force && phoenix-up'"
+echo_green_text "NOTE: Due to macOS limitations, by default, your policies will only update every 6 hours or on device boot. If you want to enforce a policies update, you can run "phoenix-up" after the update is downloaded download with Homebrew.\nYou can also set an alias in your ~/.zshrc to make this easier, such as:\nalias update='brew update && brew upgrade --force --verbose && phoenix-up'"
--- uninstaller_scripts/macos_uninstall.sh
+++ uninstaller_scripts/macos_uninstall.sh
@@ -1,28 +1,71 @@
-#!/bin/zsh
+#! /usr/bin/env bash
 
-sudo rm -f /Applications/Firefox.app/Contents/Resources/phoenix.cfg
 
-sudo rm -f /Applications/Firefox.app/Contents/Resources/defaults/pref/phoenix.js
+## Functions
+echo_red_text() {
+	echo -e "\033[31m$1\033[0m"
+}
 
-sudo rm -f /Applications/Firefox.app/Contents/Resources/mozilla.cfg
+echo_green_text() {
+	echo -e "\033[32m$1\033[0m"
+}
 
-sudo rm -f /Applications/Firefox.app/Contents/Resources/defaults/pref/local-settings.js
+error_fn() {
+	echo
+	echo -e "\033[31mSomething went wrong! The script failed.\033[0m"
+	echo -e "\033[31mPlease report this (with the output message) to https://phoenix.celenity.dev/issues\033[0m"
+	echo
+	exit 1
+}
 
-sudo launchctl unload -w  ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist
 
-sudo rm -f ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist
+## Uninstall Phoenix
+echo_green_text "Removing phoenix.cfg..."
+sudo rm -f /Applications/Firefox.app/Contents/Resources/phoenix.cfg || error_fn
+echo
 
-sudo rm -f /usr/local/sbin/update_policies_phoenix.sh
+echo_green_text "Removing phoenix.js..."
+sudo rm -f /Applications/Firefox.app/Contents/Resources/defaults/pref/phoenix.js || error_fn
+echo
 
-sudo rm -f /Applications/Firefox.app/Contents/Resources/distribution/policies.json
+echo_green_text "Removing legacy mozilla.cfg if installed..."
+sudo rm -f /Applications/Firefox.app/Contents/Resources/mozilla.cfg || error_fn
+echo
 
-brew uninstall phoenix-policies
+echo_green_text "Removing legacy local-settings.js if installed..."
+sudo rm -f /Applications/Firefox.app/Contents/Resources/defaults/pref/local-settings.js || error_fn
+echo
 
-brew untap celenity/tap
+echo_green_text "Unloading ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist..."
+sudo launchctl unload -w  ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist || error_fn
+echo
 
-# Ensure we remove legacy tap...
-brew untap celenity/Phoenix-Policies-macOS
+echo_green_text "Removing ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist..."
+sudo rm -f ~/Library/LaunchAgents/com.user.updatepoliciesphoenix.plist || error_fn
+echo
 
-brew update
+echo_green_text "Removing /usr/local/sbin/update_policies_phoenix.sh..."
+sudo rm -f /usr/local/sbin/update_policies_phoenix.sh || error_fn
+echo
 
-printf "Thanks for giving Phoenix a shot. Sorry to see you go :(. Please leave feedback on how we can improve! https://phoenix.celenity.dev/issues"
+echo_green_text "Removing policies.json..."
+sudo rm -f /Applications/Firefox.app/Contents/Resources/distribution/policies.json || error_fn
+echo
+
+echo_green_text "Uninstalling phoenix-policies..."
+brew uninstall phoenix-policies | error_fn
+echo
+
+echo_green_text "Removing celenity's Tap from Homebrew..."
+brew untap celenity/tap || error_fn
+echo
+
+echo_green_text "Removing legacy Phoenix-Policies-macOS Tap from Homebrew if installed..."
+brew untap celenity/Phoenix-Policies-macOS || error_fn
+echo
+
+echo_green_text "Updating Homebrew cache..."
+brew update || error_fn
+echo
+
+echo_green_text "Thanks for giving Phoenix a shot. Sorry to see you go :(. Please leave feedback on how we can improve! https://phoenix.celenity.dev/issues"
