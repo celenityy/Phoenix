67ce058abd59647593650e8cf2ab030702df04ff
celenity <celenity@celenity.dev>
2024-12-14 01:34:47 -0500

    Tweaks
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/phoenix.js
+++ defaults/pref/phoenix.js
@@ -1,17 +1,11 @@
 //
-pref("0.phoenix.js.initialized", true);
+pref("browser.phoenix.js.initialized", true, locked);
 // Enforce that we(or any other mozilla.cfg files) do not get privileged browser access...
 // https://www.mozilla.org/firefox/62.0/releasenotes/
-pref("general.config.sandbox_enabled", true);
+pref("general.config.sandbox_enabled", true, locked);
 
-pref("security.turn_off_all_security_so_that_viruses_can_take_over_this_computer", false);
-
-// Defense in depth, enforces users can never be identified...
-pref("mail.identity.useremail", "");
-pref("autoadmin.append_emailaddr", false);
-
-pref("general.config.filename", "phoenix.cfg");
-pref("general.config.vendor", "phoenix");
+pref("general.config.filename", "phoenix.cfg", locked);
+pref("general.config.vendor", "phoenix", locked);
 pref("general.config.obscure_value", 0);
 
-pref("0.phoenix.js.applied", true);
+pref("browser.phoenix.js.applied", true, locked);
