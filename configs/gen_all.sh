80e4344fa2e1e393b7523e3f8fde9fcca4b09225
celenity <celenity@celenity.dev>
2024-12-14 20:54:02 -0500

    Update build scripts to accomodate for recent changes, also tweak 'Firefox-UI-Fix' configs
    Signed-off-by: celenity <celenity@celenity.dev>

--- configs/gen_all.sh
+++ configs/gen_all.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-cp base.cfg manual/phoenix.cfg
+cat phoenix.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/phoenix.cfg
 
-cat base.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/firefox-ui-fix.cfg
-
-cat base.cfg configs/No-Sync/no-sync-specific.cfg > configs/No-Sync/no-sync.cfg
+cat phoenix.cfg configs/No-Sync/no-sync-specific.cfg > configs/No-Sync/no-sync.cfg
 
 cat configs/No-Sync/no-sync.cfg configs/Discord/discord-specific.cfg > configs/Discord/discord.cfg
 
-cat configs/Discord/discord.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Discord/Firefox-UI-Fix/discord.cfg
+cat configs/Discord/discord.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/discord.cfg
 
 cat configs/No-Sync/no-sync.cfg configs/YouTube/youtube-specific.cfg > configs/YouTube/youtube.cfg
 
-cat configs/YouTube/youtube.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/YouTube/Firefox-UI-Fix/youtube.cfg
+cat configs/YouTube/youtube.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/youtube.cfg
 
 cat configs/No-Sync/no-sync.cfg configs/Twitter/twitter-specific.cfg > configs/Twitter/twitter.cfg
 
-cat configs/Twitter/twitter.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Twitter/Firefox-UI-Fix/twitter.cfg
+cat configs/Twitter/twitter.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/twitter.cfg
 
-cat base.cfg configs/Hardened/hardened-specific.cfg > configs/Hardened/hardened.cfg
+cat phoenix.cfg configs/Hardened/hardened-specific.cfg > configs/Hardened/hardened.cfg
 
-cat configs/Hardened/hardened.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Hardened/Firefox-UI-Fix/hardened.cfg
+cat configs/Hardened/hardened.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/hardened.cfg
 
-cat configs/No-Sync/no-sync.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/No-Sync/Firefox-UI-Fix/no-sync.cfg
+cat configs/No-Sync/no-sync.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/no-sync.cfg
 
 cat configs/Hardened/hardened.cfg configs/No-Sync/no-sync-specific.cfg > configs/Hardened-No-Sync/hardened-no-sync.cfg
 
-cat configs/Hardened-No-Sync/hardened-no-sync.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Hardened-No-Sync/Firefox-UI-Fix/hardened-no-sync.cfg
+cat configs/Hardened-No-Sync/hardened-no-sync.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/hardened-no-sync.cfg
 
 cat configs/Hardened/hardened.cfg configs/Dev/dev-specific.cfg > configs/Dev/dev.cfg
 
-cat configs/Dev/dev.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Dev/Firefox-UI-Fix/dev.cfg
+cat configs/Dev/dev.cfg configs/Firefox-UI-Fix/firefox-ui-fix-specific.cfg > configs/Firefox-UI-Fix/dev.cfg
