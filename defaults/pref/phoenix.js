0114f2258340569466c6f48a626806ef3f7b4d92
celenity <celenity@celenity.dev>
2025-01-10 11:31:21 +0000

    Lock 'general.config.obsscure_value' - changing this will completely break Phoenix...
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/phoenix.js
+++ defaults/pref/phoenix.js
@@ -6,6 +6,6 @@ pref("general.config.sandbox_enabled", true, locked);
 
 pref("general.config.filename", "phoenix.cfg", locked);
 pref("general.config.vendor", "phoenix", locked);
-pref("general.config.obscure_value", 0);
+pref("general.config.obscure_value", 0, locked);
 
 pref("browser.phoenix.js.applied", true, locked);
