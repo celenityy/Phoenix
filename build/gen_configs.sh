972980eefaf9955d8eac90b9c3bd0070e7b85f94
celenity <celenity@celenity.dev>
2025-01-20 01:17:42 +0000

    Base specialized configs off Extended No-Sync instead of No-Sync going forward, also heavily improve build process + some additional clean-up & organization...
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/gen_configs.sh
+++ build/gen_configs.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-cat configs/no-sync.cfg build/configs/discord-spec.cfg > configs/discord.cfg
-
-cat configs/discord.cfg configs/ui-fix.cfg > configs/ui-fix/discord.cfg
-
-cat configs/no-sync.cfg build/configs/element-spec.cfg > configs/element.cfg
-
-cat configs/element.cfg configs/ui-fix.cfg > configs/ui-fix/element.cfg
-
-cat configs/no-sync.cfg build/configs/youtube-spec.cfg > configs/youtube.cfg
-
-cat configs/youtube.cfg configs/ui-fix.cfg > configs/ui-fix/youtube.cfg
-
-cat configs/no-sync.cfg build/configs/twitter-spec.cfg > configs/twitter.cfg
-
-cat configs/twitter.cfg configs/ui-fix.cfg > configs/ui-fix/twitter.cfg
-
 cat configs/hardened.cfg configs/ui-fix.cfg > configs/ui-fix/hardened.cfg
 
 cat configs/no-sync.cfg configs/ui-fix.cfg > configs/ui-fix/no-sync.cfg
@@ -23,3 +7,21 @@ cat configs/no-sync.cfg configs/ui-fix.cfg > configs/ui-fix/no-sync.cfg
 cat configs/no-sync.cfg configs/hardened.cfg > configs/hardened-no-sync.cfg
 
 cat configs/hardened-no-sync.cfg configs/ui-fix.cfg > configs/ui-fix/hardened-no-sync.cfg
+
+cat configs/hardened-no-sync.cfg build/configs/specialized-spec.cfg > build/configs/specialized-core.cfg
+
+cat build/configs/specialized-core.cfg build/configs/discord-spec.cfg > configs/discord.cfg
+
+cat configs/discord.cfg configs/ui-fix.cfg > configs/ui-fix/discord.cfg
+
+cat build/configs/specialized-core.cfg build/configs/element-spec.cfg > configs/element.cfg
+
+cat configs/element.cfg configs/ui-fix.cfg > configs/ui-fix/element.cfg
+
+cat build/configs/specialized-core.cfg build/configs/youtube-spec.cfg > configs/youtube.cfg
+
+cat configs/youtube.cfg configs/ui-fix.cfg > configs/ui-fix/youtube.cfg
+
+cat build/configs/specialized-core.cfg build/configs/twitter-spec.cfg > configs/twitter.cfg
+
+cat configs/twitter.cfg configs/ui-fix.cfg > configs/ui-fix/twitter.cfg
