8277bf999265d87d24148c34d827fb69c2dce537
celenity <celenity@celenity.dev>
2025-01-20 01:53:37 +0000

    Build Phoenix 2025.01.20.1
    Signed-off-by: celenity <celenity@celenity.dev>

--- android/phoenix.js
+++ android/phoenix.js
@@ -2,7 +2,7 @@
 // The Phoenix shall rise from the ashes of what fell before it.
 // RIP Mull.
 
-pref("browser.phoenix.version", "2025.01.19.1", locked);
+pref("browser.phoenix.version", "2025.01.20.1", locked);
 
 // Let's begin.
 
