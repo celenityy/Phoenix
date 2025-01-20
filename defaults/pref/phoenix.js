2e21eb317411535175898e073f1b6107a6211ba1
celenity <celenity@celenity.dev>
2025-01-20 03:01:36 +0000

    Minor syntax fix...
    Signed-off-by: celenity <celenity@celenity.dev>

--- defaults/pref/phoenix.js
+++ defaults/pref/phoenix.js
@@ -8,4 +8,4 @@ pref("general.config.filename", "phoenix.cfg", locked);
 pref("general.config.vendor", "phoenix", locked);
 pref("general.config.obscure_value", 0, locked);
 
-lockPref("browser.phoenix.js.status", "successfully applied :D");
+pref("browser.phoenix.js.status", "successfully applied :D", locked);
