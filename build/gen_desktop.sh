0f8ffb46a77edc3e878b0323cbcdb9e3003626ce
celenity <celenity@celenity.dev>
2025-02-07 00:25:12 -0500

    Remove deprecated/unnecessary 'No-Sync' configs
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/gen_desktop.sh
+++ build/gen_desktop.sh
@@ -10,26 +10,18 @@ python3 build/convert.py prefs/phoenix-extended-desktop.js configs/hardened.cfg
 
 cat configs/hardened.cfg configs/ui-fix.cfg > configs/ui-fix/hardened.cfg
 
-cat configs/no-sync.cfg configs/ui-fix.cfg > configs/ui-fix/no-sync.cfg
-
-cat configs/no-sync.cfg configs/hardened.cfg > configs/hardened-no-sync.cfg
-
-cat configs/hardened-no-sync.cfg configs/ui-fix.cfg > configs/ui-fix/hardened-no-sync.cfg
-
-cat configs/hardened-no-sync.cfg build/configs/specialized-spec.cfg > build/configs/specialized-core.cfg
-
-cat build/configs/specialized-core.cfg build/configs/discord-spec.cfg > configs/discord.cfg
+cat build/configs/specialized-spec.cfg build/configs/discord-spec.cfg > configs/discord.cfg
 
 cat configs/discord.cfg configs/ui-fix.cfg > configs/ui-fix/discord.cfg
 
-cat build/configs/specialized-core.cfg build/configs/element-spec.cfg > configs/element.cfg
+cat build/configs/specialized-spec.cfg build/configs/element-spec.cfg > configs/element.cfg
 
 cat configs/element.cfg configs/ui-fix.cfg > configs/ui-fix/element.cfg
 
-cat build/configs/specialized-core.cfg build/configs/youtube-spec.cfg > configs/youtube.cfg
+cat build/configs/specialized-spec.cfg build/configs/youtube-spec.cfg > configs/youtube.cfg
 
 cat configs/youtube.cfg configs/ui-fix.cfg > configs/ui-fix/youtube.cfg
 
-cat build/configs/specialized-core.cfg build/configs/twitter-spec.cfg > configs/twitter.cfg
+cat build/configs/specialized-spec.cfg build/configs/twitter-spec.cfg > configs/twitter.cfg
 
 cat configs/twitter.cfg configs/ui-fix.cfg > configs/ui-fix/twitter.cfg
