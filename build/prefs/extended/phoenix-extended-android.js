2a641b1f89910c46ac81316424363a03b9e980d4
celenity <celenity@celenity.dev>
2025-02-12 01:16:12 -0500

    Licensing woes... https://github.com/NixOS/nixpkgs/pull/380398#pullrequestreview-2604295609
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/prefs/extended/phoenix-extended-android.js
+++ build/prefs/extended/phoenix-extended-android.js
@@ -1,5 +1,17 @@
 //
 
+//
+// Copyright (C) 2024-2025 celenity
+//
+// This file is part of Phoenix.
+//
+// Phoenix is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// Phoenix is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Phoenix. If not, see https://www.gnu.org/licenses/.
+//
+
 // This file contains preferences specific to Phoenix (Extended) on Android.
 
 // 001 ADVANCED FINGERPRINTING PROTECTION
