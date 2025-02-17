401535970ac06fde175248eecd0c19fa53f65bf3
celenity <celenity@celenity.dev>
2025-02-17 01:20:49 -0500

    More testing...
    Signed-off-by: celenity <celenity@celenity.dev>

--- installer_scripts/macos/phoenix-initialize-system.sh
+++ installer_scripts/macos/phoenix-initialize-system.sh
@@ -17,20 +17,20 @@ error_fn() {
 	exit 1
 }
 
-echo_green_text "Creating /Applications/Firefox.app/Contents/Resources/defaults/pref directory..."
-sudo /bin/mkdir -v -p /Applications/Firefox.app/Contents/Resources/defaults/pref || error_fn
+echo_green_text "Creating /Applications/'Firefox Nightly'.app/Contents/Resources/defaults/pref directory..."
+sudo /bin/mkdir -v -p /Applications/'Firefox Nightly'.app/Contents/Resources/defaults/pref || error_fn
 echo
 
-echo_green_text "Changing permissions of /Applications/Firefox.app/Contents/Resources/defaults/pref to 644..."
-sudo /bin/chmod -v 644 /Applications/Firefox.app/Contents/Resources/defaults/pref || error_fn
+echo_green_text "Changing permissions of /Applications/'Firefox Nightly'.app/Contents/Resources/defaults/pref to 644..."
+sudo /bin/chmod -v 644 /Applications/'Firefox Nightly'.app/Contents/Resources/defaults/pref || error_fn
 echo
 
-echo_green_text "Copying phoenix-bootstrap.js to /Applications/Firefox.app/Contents/Resources/defaults/pref/phoenix-bootstrap.js..."
-sudo /bin/cp phoenix-bootstrap.js /Applications/Firefox.app/Contents/Resources/defaults/pref/phoenix-bootstrap.js || error_fn
+echo_green_text "Copying phoenix-bootstrap.js to /Applications/'Firefox Nightly'.app/Contents/Resources/defaults/pref/phoenix-bootstrap.js..."
+sudo /bin/cp phoenix-bootstrap.js /Applications/'Firefox Nightly'.app/Contents/Resources/defaults/pref/phoenix-bootstrap.js || error_fn
 echo
 
-echo_green_text "Copying phoenix-bootstrap.cfg to /Applications/Firefox.app/Contents/Resources/phoenix-bootstrap.cfg.."
-sudo /bin/cp phoenix-bootstrap.cfg /Applications/Firefox.app/Contents/Resources/phoenix-bootstrap.js || error_fn
+echo_green_text "Copying phoenix-bootstrap.cfg to /Applications/'Firefox Nightly'.app/Contents/Resources/phoenix-bootstrap.cfg.."
+sudo /bin/cp phoenix-bootstrap.cfg /Applications/'Firefox Nightly'.app/Contents/Resources/phoenix-bootstrap.js || error_fn
 echo
 
 echo_green_text "All done. :) Congratulations, you've successfully installed Phoenix.\nWhat comes next is for you to decide. I would strongly recommend taking a look at some of the user.js files we offer, such as our 'Hardened' option for more comprehensive protection, at the cost of minimal breakage.\nYou can learn more here https://phoenix.celenity.dev/#extended.\n"
