6e7a6c60d9532906fee582c7c92445b2007a2108
celenity <celenity@celenity.dev>
2025-02-21 21:28:47 -0500

    fix macOS builds
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/gen_macos.sh
+++ build/gen_macos.sh
@@ -2,32 +2,34 @@
 
 python3 build/convert.py prefs/phoenix-desktop.js build/configs/phoenix-spec.cfg
 
-cat build/configs/phoenix-spec.cfg configs/hardened.cfg > configs/macos/hardened.cfg
+awk '!/NO-OSX/' build/configs/phoenix-spec.cfg > macos/phoenix.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix.cfg > configs/macos/ui-fix.cfg
+cat macos/phoenix.cfg configs/hardened.cfg > configs/macos/hardened.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix/hardened.cfg > configs/macos/ui-fix/hardened.cfg
+cat macos/phoenix.cfg configs/ui-fix.cfg > configs/macos/ui-fix.cfg
 
-cat build/configs/phoenix-spec.cfg configs/apple-maps.cfg > configs/macos/apple-maps.cfg
+cat macos/phoenix.cfg configs/ui-fix/hardened.cfg > configs/macos/ui-fix/hardened.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix/apple-maps.cfg > configs/macos/ui-fix/apple-maps.cfg
+cat macos/phoenix.cfg configs/apple-maps.cfg > configs/macos/apple-maps.cfg
 
-cat build/configs/phoenix-spec.cfg configs/discord.cfg > configs/macos/discord.cfg
+cat macos/phoenix.cfg configs/ui-fix/apple-maps.cfg > configs/macos/ui-fix/apple-maps.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix/discord.cfg > configs/macos/ui-fix/discord.cfg
+cat macos/phoenix.cfg configs/discord.cfg > configs/macos/discord.cfg
 
-cat build/configs/phoenix-spec.cfg configs/element.cfg > configs/macos/element.cfg
+cat macos/phoenix.cfg configs/ui-fix/discord.cfg > configs/macos/ui-fix/discord.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix/element.cfg > configs/macos/ui-fix/element.cfg
+cat macos/phoenix.cfg configs/element.cfg > configs/macos/element.cfg
 
-cat build/configs/phoenix-spec.cfg configs/google-maps.cfg > configs/macos/google-maps.cfg
+cat macos/phoenix.cfg configs/ui-fix/element.cfg > configs/macos/ui-fix/element.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix/google-maps.cfg > configs/macos/ui-fix/google-maps.cfg
+cat macos/phoenix.cfg configs/google-maps.cfg > configs/macos/google-maps.cfg
 
-cat build/configs/phoenix-spec.cfg configs/twitter.cfg > configs/macos/twitter.cfg
+cat macos/phoenix.cfg configs/ui-fix/google-maps.cfg > configs/macos/ui-fix/google-maps.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix/twitter.cfg > configs/macos/ui-fix/twitter.cfg
+cat macos/phoenix.cfg configs/twitter.cfg > configs/macos/twitter.cfg
 
-cat build/configs/phoenix-spec.cfg configs/youtube.cfg > configs/macos/youtube.cfg
+cat macos/phoenix.cfg configs/ui-fix/twitter.cfg > configs/macos/ui-fix/twitter.cfg
 
-cat build/configs/phoenix-spec.cfg configs/ui-fix/youtube.cfg > configs/macos/ui-fix/youtube.cfg
+cat macos/phoenix.cfg configs/youtube.cfg > configs/macos/youtube.cfg
+
+cat macos/phoenix.cfg configs/ui-fix/youtube.cfg > configs/macos/ui-fix/youtube.cfg
