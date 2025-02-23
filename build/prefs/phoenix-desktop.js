4f8d1f2064143c10a14b0cdd4e0b8e177d2eb995
celenity <celenity@celenity.dev>
2025-02-23 18:18:42 -0500

    Hide the setting at `about:home` to enable "Top Stories" - `browser.newtabpage.activity-stream.feeds.system.topstories` -> `false`
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/prefs/phoenix-desktop.js
+++ build/prefs/phoenix-desktop.js
@@ -146,6 +146,7 @@ pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.ha
 pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines", "");
 pref("browser.newtabpage.activity-stream.feeds.section.topstories", false, locked);
 pref("browser.newtabpage.activity-stream.feeds.section.topstories.options", "{\"hidden\":true}", locked);
+pref("browser.newtabpage.activity-stream.feeds.system.topstories", false, locked); // Controls the visibility of the `about:home` setting
 pref("browser.newtabpage.pinned", "");
 pref("browser.partnerlink.attributionURL", "", locked);
 pref("browser.partnerlink.campaign.topsites", "", locked);
