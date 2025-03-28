0a382f68167b4a4113af4f34ae97140a7429aed3
celenity <celenity@celenity.dev>
2025-03-28 15:19:13 -0400

    Improve gen_archive.sh
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/gen_archive.sh
+++ build/gen_archive.sh
@@ -6,10 +6,20 @@
 
 # Script should be ran from inside the directory where you store Phoenix, not directly from the 'archives' or `build` folder...
 
+echo_green_text() {
+	echo -e "\033[32m$1\033[0m"
+}
+
 rm archives/phoenix.zip archives/phoenix-flatpak.zip archives/phoenix-osx.zip
+
+echo_green_text "Creating archives/phoenix.zip..."
 
 zip -r -FS archives/phoenix.zip * -x 'android/*' 'archives/*' 'assets/*' 'build/*' 'configs/macos/*' 'configs/phoenix.cfg' 'extensions/*' 'flake.*' 'installer_scripts/*' 'macos/*' 'prefs/phoenix-android.js' 'prefs/phoenix-extended*' 'uBlock/*' 'uninstaller_scripts/*' 'user.js' 'userjs/flatpak/*' 'userjs/macos/*' 'windows/*' '*.code-workspace' '.domains' '.DS_Store' '.git*' '_redirects'
 
+echo_green_text "Creating archives/phoenix-flatpak.zip..."
+
 zip -r -FS archives/phoenix-flatpak.zip * -x 'android/*' 'archives/*' 'assets/*' 'build/*' 'configs/macos/*' 'configs/phoenix.cfg' 'etc/*' 'extensions/*' 'flake.*' 'installer_scripts/*' 'macos/*' 'prefs/phoenix-android.js' 'prefs/phoenix-extended*' 'uBlock/*' 'uninstaller_scripts/*' 'user.js' 'userjs/linux/*' 'userjs/macos/*' 'windows/*' '*.code-workspace' '.domains' '.DS_Store' '.git*' '_redirects'
+
+echo_green_text "Creating archives/phoenix-osx.zip..."
 
 zip -r -FS archives/phoenix-osx.zip * -x 'android/*' 'archives/*' 'assets/*' 'build/*' 'configs/apple-maps.cfg' 'configs/discord.cfg' 'configs/element.cfg' 'configs/google-maps.cfg' 'configs/hardened.cfg' 'configs/phoenix.cfg' 'configs/twitter.cfg' 'configs/ui-fix.cfg' 'configs/ui-fix/*' 'configs/youtube.cfg' 'etc/*' 'extensions/*' 'flake.*' 'installer_scripts/*' 'macos/defaults/*' 'macos/Library/*' 'macos/migration/*' 'macos/phoenix-bootstrap.cfg' 'phoenix.cfg' 'policies.json' 'prefs/*' 'uBlock/*' 'uninstaller_scripts/*' 'user.js' 'userjs/flatpak/*' 'userjs/linux/*' 'windows/*' '*.code-workspace' '.domains' '.DS_Store' '.git*' '_redirects'
