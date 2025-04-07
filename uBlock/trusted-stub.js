27c98d0d2de1c7a0a3670708d74538c1d593cda6
celenity <celenity@celenity.dev>
2025-04-07 02:20:01 -0400

    test
    Signed-off-by: celenity <celenity@celenity.dev>

---
+++ uBlock/trusted-stub.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/// trusted-click-element.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-set-constant.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-suppress-native-method.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-override-element-method.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-prevent-dom-bypass.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-set-local-storage-item.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-prevent-xhr.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-replace-xhr-response.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-replace-fetch-response.js
+/// world ISOLATED
+/// alias trusted-rpfr.js
+(function(){})();
+
+/// trusted-replace-argument.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-set-attr.js
+/// world ISOLATED
+(function(){})();
+
+/// trusted-replace-node-text.js
+/// world ISOLATED
+/// alias trusted-rpnt.js
+/// alias replace-node-text.js
+/// alias rpnt.js
+(function(){})();
+
+/// trusted-replace-outbound-text.js
+/// alias trusted-rpot.js
+/// world ISOLATED
+(function(){})();
