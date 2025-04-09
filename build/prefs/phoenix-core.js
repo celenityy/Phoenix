f32beeaaa9a9d101b6bb4aaf000a89d024f88f6e
celenity <celenity@celenity.dev>
2025-04-09 01:48:48 -0400

    Limit probes to only what is supported on the current product - `toolkit.telemetry.testing.overrideProductsCheck` -> `false`
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/prefs/phoenix-core.js
+++ build/prefs/phoenix-core.js
@@ -158,6 +158,7 @@ pref("toolkit.telemetry.shutdownPingSender.backgroundtask.enabled", false, locke
 pref("toolkit.telemetry.shutdownPingSender.enabled", false, locked);
 pref("toolkit.telemetry.shutdownPingSender.enabledFirstSession", false, locked); // [DEFAULT]
 pref("toolkit.telemetry.testing.overridePreRelease", false, locked); // [HIDDEN] [DEFAULT] Never record extended/prelease data on release channels - https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/internals/preferences.html
+pref("toolkit.telemetry.testing.overrideProductsCheck", false, locked); // [DEFAULT] Limit probes to only what is supported on the current product - https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/internals/preferences.html
 pref("toolkit.telemetry.testing.suppressPingsender", true, locked); // [HIDDEN]
 pref("toolkit.telemetry.translations.logLevel", "Off");
 pref("toolkit.telemetry.unified", false, locked); // [DEFAULT - Android]
