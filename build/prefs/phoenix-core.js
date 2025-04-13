04e64e8c9cd80af502bf3475bce8e9b59a75da30
celenity <celenity@celenity.dev>
2025-04-13 00:14:29 -0400

    Disable automatic storage access grants - `dom.storage_access.auto_grants` -> `false` & `dom.storage_access.max_concurrent_auto_grants` -> `1`
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/prefs/phoenix-core.js
+++ build/prefs/phoenix-core.js
@@ -1279,6 +1279,8 @@ pref("network.http.network_error_logging.enabled", false); // [DEFAULT, HIDDEN -
 
 /// Disable storage access heuristics
 // https://developer.mozilla.org/docs/Web/Privacy/State_Partitioning#storage_access_heuristics
+pref("dom.storage_access.auto_grants", false); // Automatic storage access grants
+pref("dom.storage_access.max_concurrent_auto_grants", 1); // Automatic storage access grants
 pref("privacy.restrict3rdpartystorage.heuristic.navigation", false); // [DEFAULT - non-desktop Nightly] [HIDDEN - Android]
 pref("privacy.restrict3rdpartystorage.heuristic.opened_window_after_interaction", false);
 pref("privacy.restrict3rdpartystorage.heuristic.recently_visited", false);
@@ -1310,9 +1312,9 @@ pref("privacy.globalprivacycontrol.enabled", true);
 pref("privacy.globalprivacycontrol.functionality.enabled", true); // [DEFAULT - non-Thunderbird]
 pref("privacy.globalprivacycontrol.pbmode.enabled", true); // [DEFAULT - non-Thunderbird]
 
-/// Exclude third party trackers from storage access heuristics/auto grants (if enabled)
+/// Exclude third party trackers from storage access heuristics (if enabled)
 // https://developer.mozilla.org/docs/Web/Privacy/State_Partitioning#storage_access_heuristics
-pref("dom.storage_access.auto_grants.exclude_third_party_trackers", true); // [DEFAULT - Nightly]
+pref("dom.storage_access.auto_grants.exclude_third_party_trackers", true); // [DEFAULT - Nightly] Automatic storage access grants
 pref("privacy.restrict3rdpartystorage.heuristic.exclude_third_party_trackers", true); // [DEFAULT - Nightly]
 
 /// Improve built-in query stripping to be on par with LibreWolf & Brave
