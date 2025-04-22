cd6dc5f7b1496171dbfffa2ffde3709f82f5e79b
celenity <celenity@celenity.dev>
2025-04-22 15:54:25 -0400

    Fix build script to support both Linux & macOS
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,10 @@
 source build/env.sh
 
 # Update `browser.phoenix.version`
-sed -i '' "s/pref(\"browser.phoenix.version\", \".*\", locked);/pref(\"browser.phoenix.version\", \"$phoenix_version\", locked);/" build/prefs/phoenix-core.js
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/pref(\"browser.phoenix.version\", \".*\", locked);/pref(\"browser.phoenix.version\", \"$phoenix_version\", locked);/" build/prefs/phoenix-core.js
+else
+    sed -i "s/pref(\"browser.phoenix.version\", \".*\", locked);/pref(\"browser.phoenix.version\", \"$phoenix_version\", locked);/" build/prefs/phoenix-core.js
+fi
 
 ./build/gen_desktop.sh && ./build/gen_macos.sh && ./build/gen_android.sh && ./build/gen_policies.sh && ./build/gen_archive.sh
