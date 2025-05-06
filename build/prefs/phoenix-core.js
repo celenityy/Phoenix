120beb397f8dc0df580ea45e5816abd0118d08f5
celenity <celenity@celenity.dev>
2025-05-05 21:06:20 -0400

    Limit maximum cookie lifetime to 6 months/180 days (Like Brave) - `network.cookie.maxageCap` -> `15552000`
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/prefs/phoenix-core.js
+++ build/prefs/phoenix-core.js
@@ -1428,6 +1428,12 @@ pref("permissions.isolateBy.userContext", true);
 // (ex. https://searchfox.org/mozilla-central/source/toolkit/components/extensions/test/xpcshell/test_ext_contentscript_antitracking.js)
 pref("privacy.antitracking.isolateContentScriptResources", true); // [NIGHTLY]
 
+/// Limit maximum cookie lifetime to 6 months/180 days (Like Brave)
+// https://github.com/brave/brave-browser/issues/3443
+// https://github.com/fmarier/brave-core/commit/4d222df50a8dfaaabb31e9f2c5070c4db5ba8fd5
+// For testing: https://setcookie.net/
+pref("network.cookie.maxageCap", 15552000); // Firefox's default is currently 400 days (34560000)
+
 /// Prevent third parties from setting cookies unless the third party already has cookies as a first party (Like Safari)
 // https://webkit.org/tracking-prevention/#the-default-cookie-policy
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1587182
