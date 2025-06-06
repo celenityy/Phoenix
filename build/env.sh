43a83ad008b5ae8617b85eb579138df9d24d22b5
celenity <celenity@celenity.dev>
2025-06-06 01:36:53 -0400

    Build 2025.06.06.1
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/env.sh
+++ build/env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Version of Phoenix you'd like to build
-export phoenix_version=2025.06.02.2
+export phoenix_version=2025.06.06.1
 
 # Where `Phoenix` (this repo) is located
 export phoenix_dir=$(dirname $(dirname "$(realpath "$0")"))
