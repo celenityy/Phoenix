5090159fc1f3882cac9fe68ded588def766d631c
celenity <celenity@celenity.dev>
2025-06-15 02:51:30 -0400

    Disable requests to 0.0.0.0
     `network.socket.ip_addr_any.disabled` -> `true`
    
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -2798,6 +2798,12 @@ pref("network.offline-mirrors-connectivity", false); // [DEFAULT]
 pref("offline.autoDetect", false); // [LINUX-ONLY] RedHat/Fedora-specific
 pref("toolkit.networkmanager.disable", true); // [LINUX-ONLY] RedHat/Fedora-specific
 
+/// Disable requests to 0.0.0.0
+// Appears to be related to a (potentially severe?) privacy/security issue, but bug is confidential so I'm unable to find actual details...
+// This is also being set by Tor Browser
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1889130
+pref("network.socket.ip_addr_any.disabled", true); // [DEFAULT - Nightly]
+
 /// Disable weather on Firefox Home by default [NO-ANDROID] [NO-MAIL]
 pref("browser.newtabpage.activity-stream.showWeather", false); // [NO-ANDROID] [NO-MAIL]
 
