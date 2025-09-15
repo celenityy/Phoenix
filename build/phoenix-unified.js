7d1c6a47513502849fae19ee438a6e70fe78df5e
celenity <celenity@celenity.dev>
2025-09-15 16:04:10 -0400

    fix: Note that `distribution.about` is hidden (+ do not define on `Extended` for Thunderbird)
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -84,7 +84,7 @@ Unspecified = This preference should be set EVERYWHERE
 
 /// Add custom branding under `Firefox Updates` at `about:preferences#general` [NO-ANDROID] [NO-MAIL]
 // This will unfortunately only display if the version of Firefox you're using is repackaged (ex. Flatpaks/Linux distros) [NO-ANDROID] [NO-MAIL]
-pref("distribution.about", "Phoenix for Mozilla Firefox - 2025.09.07.1 💜", locked); // [NO-ANDROID] [NO-MAIL]
+pref("distribution.about", "Phoenix for Mozilla Firefox - 2025.09.07.1 💜", locked); // [NO-ANDROID] [NO-MAIL] [HIDDEN]
 
 /*** 000 ABOUT:CONFIG ***/
 
