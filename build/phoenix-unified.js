5c48e1353cb4119a83364b945af4091594350fc3
celenity <celenity@celenity.dev>
2025-10-12 16:11:36 -0400

    2025.10.12.1
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -16,7 +16,7 @@
 // Welcome to the heart of the Phoenix.
 // This file contains preferences shared across all Phoenix configs, platforms (Desktop & Android), and Dove.
 
-pref("browser.phoenix.version", "2025.10.03.1", locked);
+pref("browser.phoenix.version", "2025.10.12.1", locked);
 
 /* INDEX 
 
@@ -83,11 +83,11 @@ Unspecified = This preference should be set EVERYWHERE
 */
 
 /// Add custom branding at `about:support`
-pref("app.support.vendor", "Phoenix: 2025.10.03.1", locked); // [NO-MAIL] [HIDDEN]
+pref("app.support.vendor", "Phoenix: 2025.10.12.1", locked); // [NO-MAIL] [HIDDEN]
 
 /// Add custom branding under `Firefox Updates` at `about:preferences#general` [NO-ANDROID] [NO-MAIL]
 // This will unfortunately only display if the version of Firefox you're using is repackaged (ex. Flatpaks/Linux distros) [NO-ANDROID] [NO-MAIL]
-pref("distribution.about", "Phoenix for Mozilla Firefox - 2025.10.03.1 💜", locked); // [NO-ANDROID] [NO-MAIL] [HIDDEN]
+pref("distribution.about", "Phoenix for Mozilla Firefox - 2025.10.12.1 💜", locked); // [NO-ANDROID] [NO-MAIL] [HIDDEN]
 pref("distribution.id", "default", locked); // [LINUX-ONLY] [HIDDEN]
 pref("distribution.version", "default", locked); // [LINUX-ONLY] [HIDDEN]
 
