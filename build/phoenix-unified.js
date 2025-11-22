fdb1817d8cc149c3c6926d028843e879eeb10116
celenity <celenity@celenity.dev>
2025-11-21 19:13:29 -0500

    feat: DESKTOP - Explicitly enable Firefox Home by default (primarily to expose `browser.newtabpage.activity-stream.testing.shouldInitializeFeeds` and `browser.newtabpage.disableNewTabAsAddon` via `about:config`)
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -3947,6 +3947,11 @@ pref("browser.newtabpage.activity-stream.trendingSearch.enabled", false); // [NO
 pref("browser.newtabpage.activity-stream.newtabWallpapers.highlightDismissed", true); // [NO-ANDROID] [NO-MAIL]
 pref("browser.newtabpage.activity-stream.newtabWallpapers.highlightEnabled", false); // [NO-ANDROID] [NO-MAIL] [DEFAULT]
 
+/// Enable Firefox Home [NO-ANDROID] [NO-MAIL]
+pref("browser.newtabpage.activity-stream.testing.shouldInitializeFeeds", true); // [NO-ANDROID] [NO-MAIL] [DEFAULT] [HIDDEN] https://searchfox.org/firefox-main/rev/82e2435f/browser/extensions/newtab/lib/ActivityStream.sys.mjs#150
+pref("browser.newtabpage.disableNewTabAsAddon", false); // [NO-ANDROID] [NO-MAIL] [DEFAULT] [HIDDEN] https://searchfox.org/firefox-main/rev/82e2435f/browser/components/newtab/AboutNewTabResourceMapping.sys.mjs#186
+pref("browser.newtabpage.enabled", true); // [NO-ANDROID] [NO-MAIL] [DEFAULT]
+
 /// Enable the new shortcuts UI refresh by default [NO-ANDROID] [NO-MAIL]
 pref("browser.newtabpage.activity-stream.newtabShortcuts.refresh", true); // [NO-ANDROID] [NO-MAIL] [DEFAULT]
 
