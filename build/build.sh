b63424351675c4998ace9138fd72023506bbccd5
Seyed Mohamad Amin Modaresi <modaresisofthard@proton.me>
2025-11-26 21:25:00 +0330

    set -eu to enable debugging features for build scripts

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 source build/env.sh
 
 # Update "browser.phoenix.version"
--- build/fly.sh
+++ build/fly.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 # Welcome to the Phoenix Unified build script!
 # This script should be ran from inside the directory where you store Phoenix, not directly from the 'archives' or `build` folder...
 
