bb82bd987d8a5347d1d7213ffb8820298d51a52f
celenity <celenity@celenity.dev>
2025-11-25 21:50:33 -0500

    feat: Disable Native Messaging by default
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -2744,6 +2744,16 @@ pref("dom.security.credentialmanagement.identity.lightweight.enabled", false); /
 // https://searchfox.org/firefox-main/rev/82e2435f/widget/windows/nsClipboard.cpp#323 [WINDOWS-ONLY]
 pref("clipboard.copyPrivateDataToClipboardCloudOrHistory", false); // [WINDOWS-ONLY] [DEFAULT]
 
+/// Disable Native Messaging
+// This functionality is used to allow browser extensions to communicate with external apps/programs
+// Naturally, this raises various privacy and security concerns
+// https://developer.mozilla.org/docs/Mozilla/Add-ons/WebExtensions/Native_messaging
+// https://developer.chrome.com/docs/extensions/develop/concepts/native-messaging
+// https://searchfox.org/firefox-main/rev/af0f713f/toolkit/components/extensions/NativeMessaging.sys.mjs#12
+pref("webextensions.native-messaging.max-input-message-bytes", 0); // [HIDDEN] [DEFAULT: 1048576]
+pref("webextensions.native-messaging.max-output-message-bytes", 0); // [HIDDEN] [DEFAULT: -1]
+pref("widget.use-xdg-desktop-portal.native-messaging", 0); // [LINUX-ONLY} [DEFAULT] For Flatpak/Snap https://searchfox.org/firefox-main/source/toolkit/components/extensions/docs/native-messaging-portal-design.rst
+
 /// Disable Reporting API
 // PRIVACY: Fingerprinting concerns, used for analytics by design
 // SECURITY: Attack Surface Reduction
