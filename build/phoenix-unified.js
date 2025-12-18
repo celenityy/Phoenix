250e2a82dd4b66877e828a2c1fda0789bf62e34c
celenity <celenity@celenity.dev>
2025-12-18 00:03:49 -0500

    feat: DESKTOP - Ensure we don't prompt users to enable the AI ("key points") functionality - https://codeberg.org/librewolf/settings/pulls/98#issuecomment-9002205
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -2356,6 +2356,7 @@ pref("extensions.formautofill.ml.experiment.enabled", false); // [NO-ANDROID] [E
 /// Disable Link Preview [NO-ANDROID] [NO-MAIL]
 // Currently no-op - requires `dom.postMessage.sharedArrayBuffer.withCOOP_COEP` set to `true`: https://codeberg.org/celenity/Phoenix/issues/151 [NO-ANDROID] [NO-MAIL]
 // https://blog.mozilla.org/mozilla/ai/ai-tech/ai-link-previews-firefox/ [NO-ANDROID] [NO-MAIL]
+pref("browser.ml.linkPreview.collapsed", true); // [NO-ANDROID] [NO-MAIL] Ensure we don't prompt users to enable the AI ("key points") functionality - https://codeberg.org/librewolf/settings/pulls/98#issuecomment-9002205
 pref("browser.ml.linkPreview.enabled", false); // [NO-ANDROID] [NO-MAIL]
 pref("browser.ml.linkPreview.longPress", false); // [NO-ANDROID] [NO-MAIL] 
 pref("browser.ml.linkPreview.optin", false); // [NO-ANDROID] [NO-MAIL] [DEFAULT]
