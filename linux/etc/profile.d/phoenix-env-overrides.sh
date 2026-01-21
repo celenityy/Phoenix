f371dbdbc5fda85612e42713d2585152ca2c2d34
celenity <celenity@celenity.dev>
2026-01-21 21:09:32 +0000

    fix: update link + minor formatting tweaks for linux env variables
    Signed-off-by: celenity <celenity@celenity.dev>

--- linux/etc/profile.d/phoenix-env-overrides.sh
+++ linux/etc/profile.d/phoenix-env-overrides.sh
@@ -14,26 +14,26 @@
 # Also impacts other Mozilla software (like Thunderbird)
 
 # Disable Mozilla's ASan Crash Reporter
-## https://searchfox.org/mozilla-central/rev/a1f4cb9f/toolkit/xre/nsEmbedFunctions.cpp#261
+## https://searchfox.org/firefox-main/rev/131497bb/toolkit/xre/nsEmbedFunctions.cpp#260
 ## https://firefox-source-docs.mozilla.org/tools/sanitizer/asan_nightly.html
 ## https://github.com/choller/firefox-asan-reporter
 export MOZ_DISABLE_ASAN_REPORTER=1;
 
 # Disable Mozilla's Crash Reporter
 ## https://firefox-source-docs.mozilla.org/toolkit/crashreporter/crashreporter/index.html#user-specified-environment-variables
-export MOZ_CRASHREPORTER="";
+export MOZ_CRASHREPORTER='';
 export MOZ_CRASHREPORTER_DISABLE=1;
 export MOZ_CRASHREPORTER_NO_REPORT=1;
-export MOZ_CRASHREPORTER_URL="data;";
+export MOZ_CRASHREPORTER_URL='data;';
 
 # Disable SSLKEYLOGGING
 ## https://bugzilla.mozilla.org/show_bug.cgi?id=1183318
 ## https://bugzilla.mozilla.org/show_bug.cgi?id=1915224
-export SSLKEYLOGFILE="";
+export SSLKEYLOGFILE='';
 
 # Enable Wayland
 ## Credit to Rasmus: https://askubuntu.com/users/13884/rasmus
 ## https://askubuntu.com/questions/1456684/how-to-initialize-firefox-on-wayland-always-by-default
-if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+if [ "${XDG_SESSION_TYPE}" == 'wayland' ]; then
     export MOZ_ENABLE_WAYLAND=1
 fi
