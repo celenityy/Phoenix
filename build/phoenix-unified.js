760147b919a4d1ce8021ec03fc6b1dc88d0b3778
any1here <any1here@noreply.codeberg.org>
2026-01-21 20:08:59 +0100

    add info to network.http.referer.defaultPolicy.trackers

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -3114,6 +3114,8 @@ pref("autoadmin.append_emailaddr", false, locked); // [HIDDEN] [DEFAULT]
 pref("privacy.dynamic_firstparty.limitForeign", true);
 
 /// Restrict tracking referers
+// 0=no-referrer, 1=same-origin, 2=strict-origin-when-cross-origin (default),
+// 3=no-referrer-when-downgrade.
 pref("network.http.referer.defaultPolicy.trackers", 1);
 pref("network.http.referer.defaultPolicy.trackers.pbmode", 1);
 
