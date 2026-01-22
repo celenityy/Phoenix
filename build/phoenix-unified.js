c26729096d425d26276ff2acd4a53d52caa5e6e6
any1here <any1here@noreply.codeberg.org>
2026-01-22 15:17:11 +0100

    update note for network.dnsCacheEntries

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -3799,7 +3799,7 @@ pref("image.mem.decode_bytes_at_a_time", 65536);
 /// Increase DNS caching
 pref("network.dnsCacheExpiration", 3600); // (Default = 60)
 pref("network.dnsCacheExpirationGracePeriod", 600); // (Default = 600)
-pref("network.dnsCacheEntries", 10000); // (Default = 800)
+pref("network.dnsCacheEntries", 10000); // (Default = 1600)
 
 /// Increase the file-backed media cache size for cellular connections
 // (Default = 32768)
