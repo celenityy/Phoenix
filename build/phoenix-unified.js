8bccbe71cd7cb425443adefc73f0843f25685cb9
any1here <any1here@noreply.codeberg.org>
2026-01-22 15:37:45 +0100

    tag browser.translations.newSettingsUI.enable ESR

--- build/phoenix-unified.js
+++ build/phoenix-unified.js
@@ -3938,7 +3938,7 @@ pref("toolkit.aboutProcesses.showThreads", true); // [DEFAULT - Nightly]
 pref("browser.translations.automaticallyPopup", true); // [NO-MAIL] [DEFAULT]
 pref("browser.translations.enable", true); // [NO-MAIL] [DEFAULT - non-Thunderbird]
 pref("browser.translations.select.enable", true); // [NO-MAIL] [DEFAULT - non-Android/Thunderbird]
-pref("browser.translations.newSettingsUI.enable", true); // [NO-ANDROID] [NO-MAIL] Enables the new UI at `about:preferences#general`
+pref("browser.translations.newSettingsUI.enable", true); // [NO-ANDROID] [NO-MAIL] [ESR] Enables the new UI at `about:preferences#general`
 pref("browser.translations.simulateUnsupportedEngine", false); // [NO-MAIL] [DEFAULT]
 pref("extensions.translations.disabled", false); // [NO-ANDROID] [NO-MAIL] [HIDDEN] Enables `Exceptions` UI at `about:preferences#general`
 
