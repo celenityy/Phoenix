5502a3abdea3ff5d63b7c2d1490ca78be3d9c0e9
celenity <celenity@celenity.dev>
2026-01-25 09:15:41 +0000

    fix - DESKTOP: minor formatting for Linux env overrides
    Signed-off-by: celenity <celenity@celenity.dev>

--- build/linux/etc/profile.d/phoenix-env-overrides.sh
+++ build/linux/etc/profile.d/phoenix-env-overrides.sh
@@ -35,5 +35,5 @@ export SSLKEYLOGFILE='';
 ## Credit to Rasmus: https://askubuntu.com/users/13884/rasmus
 ## https://askubuntu.com/questions/1456684/how-to-initialize-firefox-on-wayland-always-by-default
 if [ "${XDG_SESSION_TYPE}" == 'wayland' ]; then
-    export MOZ_ENABLE_WAYLAND=1
+    export MOZ_ENABLE_WAYLAND=1;
 fi
