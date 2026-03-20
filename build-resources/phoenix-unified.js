d429f98dd952aa3216cce5f5de0e54860a5fe6aa
celenity <celenity@celenity.dev>
2026-03-20 23:44:51 +0000

    feat: Disable AI Smart Window
    Signed-off-by: celenity <celenity@celenity.dev>

--- build-resources/phoenix-unified.js
+++ build-resources/phoenix-unified.js
@@ -2454,6 +2454,7 @@ pref("browser.ai.control.linkPreviewKeyPoints", "blocked");
 pref("browser.ai.control.pdfjsAltText", "blocked");
 pref("browser.ai.control.sidebarChatbot", "blocked");
 pref("browser.ai.control.smartTabGroups", "blocked");
+pref("browser.ai.control.smartWindow", "blocked");
 pref("browser.preferences.aiControls", false); // [NO-ANDROID] [NO-MAIL] Hides the AI Controls UI settings panel (about:preferences#ai)
 
 /// Allow managing models from `about:addons`
