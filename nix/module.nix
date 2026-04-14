1e50d744fd4e7f1fa9fdee7f71c516ddd44dc16b
ivankopylov6603 <ivankopylov6603@noreply.codeberg.org>
2026-04-14 17:43:12 +0000

    fix: small whitespace changes for module.nix

--- nix/module.nix
+++ nix/module.nix
@@ -6,17 +6,17 @@
 }:
 {
   options.programs.firefox.phoenix = {
-    enable =
-      lib.mkEnableOption "Enable privacy & security hardening of Firefox using the Phoenix configs"
-      // {
-        default = true;
-      };
+    enable = lib.mkEnableOption "Enable privacy & security hardening of Firefox using the Phoenix configs" // {
+      default = true;
+    };
+
     firefoxPackages = lib.mkOption {
       type = lib.types.listOf lib.types.str;
       default = [ "firefox" ];
       description = "The name of Firefox packages of current pkgs to patch with phoenix config and policy.";
     };
   };
+
   config =
     let
       cfg = config.programs.firefox.phoenix;
@@ -28,11 +28,13 @@
           message = "Phoenix module has not been ported to nix-darwin yet. Contributions welcomed.";
         }
       ];
+
       environment.etc."firefox/defaults/pref/phoenix-desktop.js".source =
         "${pkgs.phoenix}/pref/phoenix-desktop.js";
       environment.etc."firefox/phoenix/userjs".source = "${pkgs.phoenix}/userjs";
       environment.etc."firefox/phoenix/configs".source = "${pkgs.phoenix}/configs";
       environment.etc."firefox/phoenix/assets".source = "${pkgs.phoenix}/assets";
+
       environment.variables = {
         MOZ_CRASHREPORTER = "";
         MOZ_CRASHREPORTER_DISABLE = 1;
@@ -45,14 +47,17 @@
         PHOENIX_HOST_NIX = 1;
         SSLKEYLOGFILE = "";
       };
+
       programs.firefox.policies =
         (builtins.fromJSON (builtins.readFile "${pkgs.phoenix}/policies.json")).policies;
+
       nixpkgs.overlays = [
         (import ./overlay.nix)
-        (
-          final: prev:
+        (final: prev:
           builtins.listToAttrs (
-            map (p: lib.nameValuePair p (final.withPhoenix prev.${p})) cfg.firefoxPackages
+            map
+              (p: lib.nameValuePair p (final.withPhoenix prev.${p}))
+              cfg.firefoxPackages
           )
         )
       ];
