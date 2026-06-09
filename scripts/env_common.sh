a35a79b0b4186daff6d27080532cacb1a16d3326
celenity <celenity@celenity.dev>
2026-06-09 00:57:38 +0000

    fix: update curl flags
    Notably:
    
    - Splits ciphers into separate categories for ones that should be applied to TLS 1.3 and ones that should be applied to non-TLS 1.3 connections (because curl requires/uses two separate options for this, so this ensures it handles/sets them properly)
    - Removes options that do not apply to HTTP(S) requests
    - Enables HTTP3
    - Enables parallel downloads
    - Prevents curl from automatically converting POST requests to GET requests upon certain redirects
    - Sets curl to retry upon all errors (will fail after 5 tries)
    
    Signed-off-by: celenity <celenity@celenity.dev>

--- scripts/env_common.sh
+++ scripts/env_common.sh
@@ -257,12 +257,29 @@ readonly PHOENIX_UV_TOOLS
 export PHOENIX_UV_TOOLS
 
 # Cipher suites
-## (This enforces strong cipher suites - see ex. https://browserleaks.com/tls)
-readonly PHOENIX_CIPHERS_DEFAULT='TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384'
-if [[ -z "${PHOENIX_CIPHERS+x}" ]]; then
-  PHOENIX_CIPHERS="${PHOENIX_CIPHERS_DEFAULT}"
+## (These enforce strong cipher suites - see ex. https://browserleaks.com/tls)
+
+## For TLS 1.3 connections
+### https://curl.se/docs/manpage.html#--tls13-ciphers
+readonly PHOENIX_TLS13_CIPHERS_DEFAULT='TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384'
+if [[ -z "${PHOENIX_TLS13_CIPHERS+x}" ]]; then
+  PHOENIX_TLS13_CIPHERS="${PHOENIX_TLS13_CIPHERS_DEFAULT}"
 fi
-readonly PHOENIX_CIPHERS
+readonly PHOENIX_TLS13_CIPHERS
+export PHOENIX_TLS13_CIPHERS
+
+## For non-TLS 1.3 connections
+### https://curl.se/docs/manpage.html#--ciphers
+readonly PHOENIX_NONTLS13_CIPHERS_DEFAULT='ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384'
+if [[ -z "${PHOENIX_NONTLS13_CIPHERS+x}" ]]; then
+  PHOENIX_NONTLS13_CIPHERS="${PHOENIX_NONTLS13_CIPHERS_DEFAULT}"
+fi
+readonly PHOENIX_NONTLS13_CIPHERS
+export PHOENIX_NONTLS13_CIPHERS
+
+# This includes all ciphers (combining PHOENIX_TLS13_CIPHERS + PHOENIX_NONTLS13_CIPHERS)
+## Useful because many programs do not require specifying a separate set of ciphers for TLS 1.3 like curl does
+readonly PHOENIX_CIPHERS="${PHOENIX_TLS13_CIPHERS}:${PHOENIX_NONTLS13_CIPHERS}"
 export PHOENIX_CIPHERS
 
 # If curl flags are added, this determines whether they should be appended to our default flags (default),
@@ -275,7 +292,7 @@ readonly PHOENIX_CURL_FLAGS_OVERRIDE
 export PHOENIX_CURL_FLAGS_OVERRIDE
 
 # curl flags
-readonly PHOENIX_CURL_FLAGS_DEFAULT="--disable --no-netrc --ciphers ${PHOENIX_CIPHERS} --clobber --create-dirs --delegation none --disallow-username-in-url --doh-cert-status --fail --ftp-create-dirs --ftp-ssl-control --junk-session-cookies --no-basic --no-ca-native --no-digest --no-doh-insecure --no-http0.9 --no-insecure --no-proxy-insecure --no-negotiate --no-ntlm --no-proxy-basic --no-proxy-ca-native --no-proxy-digest --no-proxy-insecure --no-proxy-ssl-allow-beast --no-proxy-ssl-auto-client-cert --no-sessionid --no-skip-existing --no-ssl --no-ssl-allow-beast --no-ssl-auto-client-cert --no-ssl-no-revoke --no-ssl-revoke-best-effort --no-tls-earlydata --no-xattr --progress-meter --proto -all,https --proto-default https --proto-redir -all,https --referer "" --remove-on-error --show-error --ssl-reqd --tlsv1.2 --trace-time --user-agent "" --verbose"
+readonly PHOENIX_CURL_FLAGS_DEFAULT="--disable --no-netrc --ciphers ${PHOENIX_NONTLS13_CIPHERS} --clobber --create-dirs --delegation none --disallow-username-in-url --doh-cert-status --fail --fail-early --http3 --junk-session-cookies --no-basic --no-ca-native --no-digest --no-doh-insecure --no-http0.9 --no-insecure --no-negotiate --no-ntlm --no-proxy-basic --no-proxy-ca-native --no-proxy-digest --no-proxy-insecure --no-proxy-negotiate --no-proxy-ssl-auto-client-cert --no-sessionid --no-skip-existing --no-ssl-auto-client-cert --no-ssl-no-revoke --no-ssl-revoke-best-effort --no-tls-earlydata --no-xattr --parallel --post301 --post302 --post303 --progress-meter --proto -all,https --proto-default https --proto-redir -all,https --proxy-ciphers ${PHOENIX_NONTLS13_CIPHERS} --proxy-tls13-ciphers ${PHOENIX_TLS13_CIPHERS} --referer '' --remove-on-error --retry 5 --retry-all-errors --retry-connrefused --show-error --tls13-ciphers ${PHOENIX_TLS13_CIPHERS} --tlsv1.2 --trace-time --user-agent '' --verbose"
 if [[ -z "${PHOENIX_CURL_FLAGS+x}" ]]; then
   readonly PHOENIX_CURL_FLAGS="${PHOENIX_CURL_FLAGS_DEFAULT}"
 elif [[ "${PHOENIX_CURL_FLAGS_OVERRIDE}" == 1 ]]; then
